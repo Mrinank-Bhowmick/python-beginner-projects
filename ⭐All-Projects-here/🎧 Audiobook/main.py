a0c4582fd7722e2d8a41ecd03f2f798bc1b75623
EFFLUX <bhowmickmrinank@gmail.com>
2022-04-25 12:23:04 +0530

    Added project

---
+++ ⭐All-Projects-here/🎧 Audiobook/main.py
@@ -0,0 +1,16 @@
+# Enter all the required modules
+
+import pyttsx3
+import PyPDF2
+
+book = open('oop.pdf', 'rb')  # Enter your PDF file path
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+
+speaker = pyttsx3.init()
+
+for num in range(7, pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
