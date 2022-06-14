edb1d22f839d78fa1e9a90aa2dee32565ce7bd0f
EFFLUX <bhowmickmrinank@gmail.com>
2022-06-14 17:00:22 +0530

    Added new programs

---
+++ All-Projects-here ⭐/Desktop-Notification/main.py
@@ -0,0 +1,9 @@
+# import win10toast
+
+from win10toast import ToastNotifier
+
+# create an object to ToastNotifier class
+
+n = ToastNotifier()
+
+n.show_toast("Python project" , "Here is your notification body" , duration = 20 , icon_path ="logo.ico")
---
+++ All-Projects-here ⭐/Translate-MorseCode/main.py
@@ -0,0 +1,89 @@
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+ 
+# Function to encrypt the string
+# according to the morse code chart
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+ 
+            # Looks up the dictionary and adds the
+            # corresponding morse code
+            # along with a space to separate
+            # morse codes for different characters
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+            # 1 space indicates different characters
+            # and 2 indicates different words
+            cipher += ' '
+ 
+    return cipher
+ 
+# Function to decrypt the string
+# from morse to english
+def decrypt(message):
+ 
+    # extra space added at the end to access the
+    # last morse code
+    message += ' '
+ 
+    decipher = ''
+    citext = ''
+    for letter in message:
+ 
+        # checks for space
+        if (letter != ' '):
+ 
+            # counter to keep track of space
+            i = 0
+ 
+            # storing morse code of a single character
+            citext += letter
+ 
+        # in case of space
+        else:
+            # if i = 1 that indicates a new character
+            i += 1
+ 
+            # if i = 2 that indicates a new word
+            if i == 2 :
+ 
+                 # adding space to separate words
+                decipher += ' '
+            else:
+ 
+                # accessing the keys using their values (reverse of encryption)
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
+                .values()).index(citext)]
+                citext = ''
+ 
+    return decipher
+ 
+# Hard-coded driver function to run the program
+def main():
+    message = "Mrinank-Bhowmick"
+    result = encrypt(message.upper())
+    print (result)
+ 
+    message = "-- .-. .. -. .- -. -.- -....- -... .... --- .-- -- .. -.-. -.-"
+    result = decrypt(message)
+    print (result)
+ 
+# Executes the main function
+if __name__ == '__main__':
+    main()
