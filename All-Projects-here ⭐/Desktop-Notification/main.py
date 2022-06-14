3b225eee14013ac1693e9ad9be350ed369067070
Lint Action <lint-action@samuelmeuli.com>
2022-06-14 11:33:25 +0000

    Fix code style issues with Black

--- All-Projects-here ⭐/Desktop-Notification/main.py
+++ All-Projects-here ⭐/Desktop-Notification/main.py
@@ -6,4 +6,9 @@ from win10toast import ToastNotifier
 
 n = ToastNotifier()
 
-n.show_toast("Python project" , "Here is your notification body" , duration = 20 , icon_path ="logo.ico")
+n.show_toast(
+    "Python project",
+    "Here is your notification body",
+    duration=20,
+    icon_path="logo.ico",
+)
