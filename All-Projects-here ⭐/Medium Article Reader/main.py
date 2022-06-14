5345bef50af71e53c766a1d3bd466c89ef4e4735
EFFLUX <bhowmickmrinank@gmail.com>
2022-06-14 15:38:55 +0530

    Added new program

---
+++ All-Projects-here ⭐/Medium Article Reader/main.py
@@ -0,0 +1,21 @@
+import pyttsx3
+import requests
+from bs4 import BeautifulSoup
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+  engine.say(audio)
+  engine.runAndWait()
+
+text = str(input("Paste article\n"))
+res = requests.get(text)
+soup = BeautifulSoup(res.text,'html.parser')
+articles = []
+for i in range(len(soup.select('.p'))):
+    article = soup.select('.p')[i].getText().strip()
+    articles.append(article)
+text = " ".join(articles)
+speak(text)
