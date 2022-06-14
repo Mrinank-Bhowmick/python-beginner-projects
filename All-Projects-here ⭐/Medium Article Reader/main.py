302cd755d0da40518cacb9a520870a58d1af0cd4
Lint Action <lint-action@samuelmeuli.com>
2022-06-14 10:09:29 +0000

    Fix code style issues with Black

--- All-Projects-here ⭐/Medium Article Reader/main.py
+++ All-Projects-here ⭐/Medium Article Reader/main.py
@@ -2,20 +2,22 @@ import pyttsx3
 import requests
 from bs4 import BeautifulSoup
 
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+
 
 def speak(audio):
-  engine.say(audio)
-  engine.runAndWait()
+    engine.say(audio)
+    engine.runAndWait()
+
 
 text = str(input("Paste article\n"))
 res = requests.get(text)
-soup = BeautifulSoup(res.text,'html.parser')
+soup = BeautifulSoup(res.text, "html.parser")
 articles = []
-for i in range(len(soup.select('.p'))):
-    article = soup.select('.p')[i].getText().strip()
+for i in range(len(soup.select(".p"))):
+    article = soup.select(".p")[i].getText().strip()
     articles.append(article)
 text = " ".join(articles)
 speak(text)
