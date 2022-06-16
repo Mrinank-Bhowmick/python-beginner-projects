e3390a3fd27261f2198150aebe39a599aaa17041
Lint Action <lint-action@samuelmeuli.com>
2022-06-16 09:53:56 +0000

    Fix code style issues with Black

--- Make-API/app.py
+++ Make-API/app.py
@@ -4,12 +4,14 @@ import random
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def life():
-    fin=open('Make-API//quote.txt','r')
-    Random_line=random.randint(0,2)
-    quote=fin.readlines()[Random_line]
+    fin = open("Make-API//quote.txt", "r")
+    Random_line = random.randint(0, 2)
+    quote = fin.readlines()[Random_line]
     return jsonify(quote[0:-1])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
