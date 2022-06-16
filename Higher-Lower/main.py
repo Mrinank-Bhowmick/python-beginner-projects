b791907f9ec2097786e72650ca45f81301753a1e
Zackery .R. Smith <72983221+ZackeryRSmith@users.noreply.github.com>
2022-06-16 15:57:54 -0400

    Add files via upload

---
+++ Higher-Lower/main.py
@@ -0,0 +1,10 @@
+from random import randint
+
+rnum = randint(0, 100) 
+noguesses = 0
+while True:
+    guess = int(input(": "))
+    if guess > rnum: print("Lower")
+    elif guess < rnum: print("Higher")
+    else: (print("You win ({noguesses} guesses used)"), quit())
+    noguesses+=1
