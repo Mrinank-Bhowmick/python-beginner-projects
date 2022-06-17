0c1b1d3e3d5f6c76b075b65ab354a715f8dfcaf9
Lint Action <lint-action@samuelmeuli.com>
2022-06-17 05:53:03 +0000

    Fix code style issues with Black

--- projects/Higher-Lower/main.py
+++ projects/Higher-Lower/main.py
@@ -1,10 +1,13 @@
 from random import randint
 
-rnum = randint(0, 100) 
+rnum = randint(0, 100)
 noguesses = 0
 while True:
     guess = int(input(": "))
-    if guess > rnum: print("Lower")
-    elif guess < rnum: print("Higher")
-    else: (print("You win ({noguesses} guesses used)"), quit())
-    noguesses+=1
+    if guess > rnum:
+        print("Lower")
+    elif guess < rnum:
+        print("Higher")
+    else:
+        (print("You win ({noguesses} guesses used)"), quit())
+    noguesses += 1
--- projects/Rock_Paper_Scissors/main.py
+++ projects/Rock_Paper_Scissors/main.py
@@ -1,10 +1,18 @@
 while True:
-    choice, bchoice, value = input("[R]ock, [P]aper, [S]cissors\n: ").lower(), __import__("random").randint(1, 3), 0
-    if choice in ["r", "p", "s"]: break
+    choice, bchoice, value = (
+        input("[R]ock, [P]aper, [S]cissors\n: ").lower(),
+        __import__("random").randint(1, 3),
+        0,
+    )
+    if choice in ["r", "p", "s"]:
+        break
     print("Not r, p, or s\n")
 
-value = ["r", "p", "s"].index(choice)+1
+value = ["r", "p", "s"].index(choice) + 1
 
-if str(value)+str(bchoice) in ["13", "21", "32"]: print("You win!")
-elif str(value) == str(bchoice):                  print("It's a draw!")
-else:                                             print("You lose.")
+if str(value) + str(bchoice) in ["13", "21", "32"]:
+    print("You win!")
+elif str(value) == str(bchoice):
+    print("It's a draw!")
+else:
+    print("You lose.")
