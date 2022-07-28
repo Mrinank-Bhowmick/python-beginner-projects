2f84dd2886063d7b3b1b2859bc22840adefe632d
Lint Action <lint-action@samuelmeuli.com>
2022-07-28 10:14:45 +0000

    Fix code style issues with Black

--- projects/Browser/main.py
+++ projects/Browser/main.py
@@ -4,8 +4,8 @@ from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 import sys
 
-class MainWindow(QMainWindow):
 
+class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.browser = QWebEngineView()
@@ -15,15 +15,15 @@ class MainWindow(QMainWindow):
         navbar = QToolBar()
         navbar.adjustSize()
         self.addToolBar(navbar)
-        back_btn = QAction('⮜', self)
+        back_btn = QAction("⮜", self)
         back_btn.triggered.connect(self.browser.back)
         navbar.addAction(back_btn)
 
-        forward_btn = QAction('⮞', self)
+        forward_btn = QAction("⮞", self)
         forward_btn.triggered.connect(self.browser.forward)
         navbar.addAction(forward_btn)
 
-        reload_btn = QAction('⟳', self)
+        reload_btn = QAction("⟳", self)
         reload_btn.triggered.connect(self.browser.reload)
         navbar.addAction(reload_btn)
 
@@ -32,7 +32,6 @@ class MainWindow(QMainWindow):
         navbar.addWidget(self.url_bar)
         self.browser.urlChanged.connect(self.update_url)
 
-  
     def open_url(self):
         url = self.url_bar.text()
         self.browser.setUrl(QUrl(url))
