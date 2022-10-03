c392ac130845fc011f45be2a96d325b3c4a4870b
Konran <94843702+Adhiraj-11@users.noreply.github.com>
2022-10-03 23:46:00 +0530

    Added a random walk project
    Using turtle, to make random patterns.

---
+++ projects/Turtle Pattern/randomPattern.py
@@ -0,0 +1,43 @@
+from turtle import *
+import turtle as t
+import random
+import time
+
+tim = t.Turtle()
+
+t.colormode(255)
+def random_colour():
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+
+    return r,g,b
+
+def turtle_movement():
+    tim.pensize(10)
+    rgb = random_colour()
+    direction = random.randint(1, 4)
+    tim.pencolor(rgb[0],rgb[1],rgb[2])
+    if direction  == 1:
+        tim.left(90)
+        tim.forward(30)
+        time.sleep(.5)
+    elif direction == 2:
+        tim.right(90)
+        tim.forward(30)
+        time.sleep(.5)
+    elif direction == 3:
+        tim.forward(30)
+        time.sleep(.5)
+    elif direction == 4:
+        tim.backward(30)
+        time.sleep(.5)
+
+colours=["GreenYellow","Chartreuse","Tan","LightSkyBlue","DeepPink","MediumSlateBlue"]
+
+t.pensize(10)
+for _ in range(0,200):
+    turtle_movement()
+
+screen=Screen()
+screen.exitonclick()
