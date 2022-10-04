e66111e90644a7a8e12d4e1747c7389ded330522
Vidit <viditk0812@gmail.com>
2022-10-04 16:50:50 +0530

    CountDown

---
+++ projects/Countdown/countdown.py
@@ -0,0 +1,17 @@
+import time
+
+
+def countdown(time_sec):
+    while time_sec:
+        mins, secs = divmod(time_sec, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        time_sec -= 1
+
+    print("Time Up")
+
+
+t = input("Enter the time in seconds: ")
+
+countdown(int(t))
