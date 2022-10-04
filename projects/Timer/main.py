9e895bb835c14ce88e182be7f87c97d0899287f8
CJ Praveen <cjpraveen@hotmail.com>
2022-10-04 08:39:05 +0530

    Update main.py

--- projects/Timer/main.py
+++ projects/Timer/main.py
@@ -1,13 +1,22 @@
+# create a timer
+
+# from time module import sleep
 from time import sleep
 
+# get the duration from user
 duration = int(input("Enter the duration in seconds: "))
+
+# The current duration 
 count = 0
 
-while True:
-    if count == duration:
-        print("Your time is up!")
-        break
-    else:
-        print(count, end="\r")
-        sleep(1)
-        count += 1
+# This loop run until the test condition is false
+while count < duration:
+    # prints the current duration
+    print(count, end="\r")
+    # sleep for 1 second
+    sleep(1)
+    # update the current duration
+    count += 1
+
+# prints if the time is up
+print("Your time is up!")
