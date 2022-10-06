f8a9583a0dc0ef949cb430827642f82cf89b6938
Lint Action <lint-action@samuelmeuli.com>
2022-10-06 05:45:02 +0000

    Fix code style issues with Black

--- projects/Captcha_Genrator/Captcha_Genrator.py
+++ projects/Captcha_Genrator/Captcha_Genrator.py
@@ -12,45 +12,50 @@ from tkinter import messagebox
 import string
 from captcha.image import ImageCaptcha
 
-image = ImageCaptcha(fonts=['C:/Users/Administrator/Downloads/ChelseaMarketsr.ttf', 'C:/Users/Administrator/Downloads/DejaVuSanssr.ttf'])
+image = ImageCaptcha(
+    fonts=[
+        "C:/Users/Administrator/Downloads/ChelseaMarketsr.ttf",
+        "C:/Users/Administrator/Downloads/DejaVuSanssr.ttf",
+    ]
+)
 
-random=str(randint(100000,999999))
+random = str(randint(100000, 999999))
 data = image.generate(random)
 assert isinstance(data, BytesIO)
-image.write(random,'out.png')
+image.write(random, "out.png")
+
 
 def verify():
     global random
-    x=t1.get("0.0",END)
-    if (int(x)==int(random)):
+    x = t1.get("0.0", END)
+    if int(x) == int(random):
         messagebox.showinfo("sucsess", "verified")
     else:
         messagebox.showinfo("Alert", "Not verified")
         refresh()
 
+
 def refresh():
-        random=str(randint(100000,999999))
-        data = image.generate(random)
-        assert isinstance(data, BytesIO)
-        image.write(random,'out.png')
-        photo = PhotoImage(file="out.png")
-        l1.config(image=photo,height=100,width=200)
-        l1.update()
-        UpdateLabel()
-    
-root=Tk()
+    random = str(randint(100000, 999999))
+    data = image.generate(random)
+    assert isinstance(data, BytesIO)
+    image.write(random, "out.png")
+    photo = PhotoImage(file="out.png")
+    l1.config(image=photo, height=100, width=200)
+    l1.update()
+    UpdateLabel()
+
+
+root = Tk()
 photo = PhotoImage(file="out.png")
 
-l1=Label(root,image=photo,height=100,width=200)
-t1=Text(root,height=5,width=50)
-b1=Button(root,text="submit",command=verify)
-b2=Button(root,text="refresh",command=refresh)
+l1 = Label(root, image=photo, height=100, width=200)
+t1 = Text(root, height=5, width=50)
+b1 = Button(root, text="submit", command=verify)
+b2 = Button(root, text="refresh", command=refresh)
 
 l1.pack()
 t1.pack()
 b1.pack()
 b2.pack()
 root.mainloop()
-
-
-
