75c7033546f1a08f27ede0c85fb236fc11a9031f
Lint Action <lint-action@samuelmeuli.com>
2022-10-09 10:30:54 +0000

    Fix code style issues with Black

--- TextDetection/TextDetection.py
+++ TextDetection/TextDetection.py
@@ -2,11 +2,13 @@ import argparse
 import io
 from google.cloud import vision
 from google.cloud.vision import types
+
+
 def main(image_file):
-    #Instantiates a client
+    # Instantiates a client
     client = vision.ImageAnnotatorClient()
     # Loads the image into memory
-    with io.open(image_file, 'rb') as image_file:
+    with io.open(image_file, "rb") as image_file:
         content = image_file.read()
     image = types.Image(content=content)
     # Perform label detection on the image file
@@ -14,8 +16,10 @@ def main(image_file):
     labels = response.text_annotations
     for label in labels:
         print(label.description)
-if __name__ == '__main__':
-    parser =argparse._ArgumentParser()
-    parser.add_argument('image_file', help='The image you\'d like to label.')    
+
+
+if __name__ == "__main__":
+    parser = argparse._ArgumentParser()
+    parser.add_argument("image_file", help="The image you'd like to label.")
     args = parser.parse_args()
-    main(args.image_file)        
+    main(args.image_file)
--- projects/Image-to-art/ascii_art.py
+++ projects/Image-to-art/ascii_art.py
@@ -1,11 +1,13 @@
 from PIL import Image
 
-ascii_characters_by_surface = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+ascii_characters_by_surface = (
+    '`^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+)
 
 
 def main():
-    image = Image.open('test1.jpg')
-    image = image.resize(( 100, 100 ))
+    image = Image.open("test1.jpg")
+    image = image.resize((100, 100))
     # you can first resize the image if needed
     # image = image.resize((width, height))
     ascii_art = convert_to_ascii_art(image)
@@ -16,7 +18,7 @@ def convert_to_ascii_art(image):
     ascii_art = []
     (width, height) = image.size
     for y in range(0, height - 1):
-        line = ''
+        line = ""
         for x in range(0, width - 1):
             px = image.getpixel((x, y))
             line += convert_pixel_to_character(px)
@@ -37,9 +39,9 @@ def save_as_text(ascii_art):
     with open("image.txt", "w") as file:
         for line in ascii_art:
             file.write(line)
-            file.write('\n')
+            file.write("\n")
         file.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
