fc481c3855ad2d61e86b44a4fcb7ae3521d82bea
Lint Action <lint-action@samuelmeuli.com>
2022-10-09 12:29:30 +0000

    Fix code style issues with Black

--- projects/Split_Tip/Tip_calculator.py
+++ projects/Split_Tip/Tip_calculator.py
@@ -1,9 +1,9 @@
 print("Welcome to the Tip calculator")
-amount= float(input("Enter your bill amount ($): "))
-tip= float(input("what percentage of tip you want to give? 5, 10, 12 or 15?\n"))
-split= int(input("How many people to split the bill amount: "))
-w=((0.01*tip)*amount)//split
-x= (amount + (0.01*tip)*amount)//split
+amount = float(input("Enter your bill amount ($): "))
+tip = float(input("what percentage of tip you want to give? 5, 10, 12 or 15?\n"))
+split = int(input("How many people to split the bill amount: "))
+w = ((0.01 * tip) * amount) // split
+x = (amount + (0.01 * tip) * amount) // split
 amount = float(input("Enter your bill amount ($): "))
 tip = float(input("what percentage of tip you want to give? 5, 10, 12 or 15?\n"))
 split = int(input("How many people to split the bill amount: "))
