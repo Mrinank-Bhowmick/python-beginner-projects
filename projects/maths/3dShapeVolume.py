25f7f55cba78dca20e028400e72fb2197f647305
ChefYeshpal <choudharydhruva@gmail.com>
2022-10-11 17:18:34 +0530

    added new shapes and fixed a loop

--- projects/maths/3dShapeVolume.py
+++ projects/maths/3dShapeVolume.py
@@ -2,12 +2,18 @@
 #updates:
     #1. 2022-10-04 12:13:15 : created this project
     #2. 2022-10-04 14:47:13 : added 6 shapes (for now)
-
+    #3. 2022-10-06 15:12:00 : Added 2 new shapes
+    #4. 2022-10-11 17:11:45 : fixed the invalid loop
 #to do
-    #Fix the "invalid input" loop further
-    #Add more shapes
-    #make easier selection for shapes
-#Welcome to my attempt to Create a volume finder for 6 specific shapes
+    
+    
+
+###############################################################################
+
+#Welcome to my attempt to Create a volume finder for 6 specific shapes!!  (more coming soon)
+
+from cmath import sqrt
+
 
 print("----------------")
 print("created by: ")
@@ -28,6 +34,8 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+
+
 print("-----------------------")
 print("Select operation.")
 print("1.Cube/cuboid")
@@ -36,9 +44,11 @@ print("3.Cylinder")
 print("4.Sphere")
 print("5.Cone")
 print("6.Ellipsoid")
+print("7.Torus(Donut)")
+print("8.Hexagonal Prism")
 print("-----------------------")
 while True:
-    choice = input("Enter choice(1/2/3/4/5/6): ")
+    choice = input("Enter choice(1/2/3/4/5/6/7/8): ")
     print("-----------------------")
 
     if choice in ('1', '2'):
@@ -59,15 +69,24 @@ while True:
             R = float(input("Enter the raduis: "))
             H = float(input("Enter the height: "))
             print("-----------------------")
-            
 
+            
             if choice == '3':
+                print("Finding the volume for Cylinder")
+                print("-----------------------")
+
                 print("The volume of the Cylinder is: ",22/7*((R)**2)*H, "unit's")
 
             elif choice == '4':
+                print("Finding the volume for Sphere")
+                print("-----------------------")
+
                 print("The volume of the Sphere is: ",4/3*22/7*((R)**3), "unit's")
 
             elif choice == '5':
+                print("Finding the volume of Cone")
+                print("-----------------------")
+
                 print("The volume of the cone is: ", 22/7*((R)**2)*H/3)
     if choice in ('6'):
             Axs = float(input("a axis: "))
@@ -79,18 +98,29 @@ while True:
             if choice == '6':
                 print("The volume of the Ellipsoid is: ",4/3 * 22/7 * Axs * Bxs * Cxs, "unit's")
 
+    if choice in ('7'):
+            R = float(input("Enter the Major Radius: "))
+            print("Make sure that what Major Radius > Minor Radius")
+            r = float(input("Enter the Minor Radius: "))
+
+            print("-----------------------")
+            
+            print("The volume of the Torus is: ", ((22/7*((r)**2))*(2*22/7*R)), "unit's")
+
+            print("-----------------------")
+
+    if choice in ('8'):
+            a = float(input("Enter the Base edge: "))
+            h = float(input("Enter the Height: "))
+
+            print("-----------------------")
+
+            print("The volume of the Right Regular Hexagonal Prism is:", ( 3 * sqrt(3)/2 * a * a * h), "unit's")
+
+    else:
+        print("Invalid Input")
+
+print("thank you for using my program!!")
 
 
-            print("-----------------------")    
-    break
-else:
-    print("Invalid Input")
 
-
-
-#insert from line 50
-'''if choice == '1':
-            print("The volume of the Cube/cuboid is: ",num1 * num2 * num3, "unit's")
-
-        elif choice == '2':
-            print("The volume of the Pyramid is: ",(num1 * num2 * num3)/3, "unit's")'''
