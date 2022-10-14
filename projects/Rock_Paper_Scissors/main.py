c4dfaeb4389119607783d42892b05fde19e6ba6e
Guillotine189 <114574134+Guillotine189@users.noreply.github.com>
2022-10-14 23:44:04 +0530

    Update main.py

--- projects/Rock_Paper_Scissors/main.py
+++ projects/Rock_Paper_Scissors/main.py
@@ -7,28 +7,29 @@ def main():
     attempts = 0
 
     while True:
-        your_choice = int(input("\n1. Rock, 2. Paper, 3. Scissors, 4. Exit: "))
+        try:
+            your_choice = int(input("\n1. Rock, 2. Paper, 3. Scissors, 4. Exit: "))
 
-        if your_choice == 4:
-            break
-        if your_choice not in {1, 2, 3}:
+            if your_choice == 4:
+                break
+
+
+            computer_choice = randint(1, 3)
+            attempts += 1
+            print(
+                f"You chose {choices[your_choice]} and computer chose {choices[computer_choice]}"
+            )
+
+            if your_choice == computer_choice:
+                print("Its a draw.")
+                attempts -= 1
+            elif str(your_choice) + str(computer_choice) in ["13", "21", "32"]:
+                print("You Win!")
+                score += 1
+            else:
+                print("You Lose.")
+        except:
             print("Invalid choice.")
-            break
-
-        computer_choice = randint(1, 3)
-        attempts += 1
-        print(
-            f"You chose {choices[your_choice]} and computer chose {choices[computer_choice]}"
-        )
-
-        if your_choice == computer_choice:
-            print("Its a draw.")
-            attempts -= 1
-        elif str(your_choice) + str(computer_choice) in ["13", "21", "32"]:
-            print("You Win!")
-            score += 1
-        else:
-            print("You Lose.")
 
     print(f"Your score is {score}/{attempts}")
     print("Thanks for playing.")
