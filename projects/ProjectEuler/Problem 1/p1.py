cbabce41570f5bd2bc6c01b93cd88ebb38cd6d10
mojisola <ibraheemzainabmojisola@gmail.com>
2022-10-17 15:34:45 +0100

    Added a Docstring

--- projects/ProjectEuler/Problem 1/p1.py
+++ projects/ProjectEuler/Problem 1/p1.py
@@ -3,7 +3,7 @@
 
 def compute():
     '''Computes the sum of the multiples af 3 and 5 between 0 and 1000'''
-    
+
     ans = sum(x for x in range(1000) if (x % 3 == 0 or x % 5 == 0))
     return str(ans)
 
