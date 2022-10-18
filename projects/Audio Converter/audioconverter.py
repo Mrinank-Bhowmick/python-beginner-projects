40a499f8fd37a077fc3bb90d8e309eea85a2f625
guiszk <guiszk@protonmail.com>
2022-10-18 00:53:28 -0300

    Added comments to Audio Converter/audioconverter.py

--- projects/Audio Converter/audioconverter.py
+++ projects/Audio Converter/audioconverter.py
@@ -7,13 +7,20 @@ if(len(sys.argv) != 3):
 
 filename = sys.argv[1]
 frm = sys.argv[2]
+
+#get absolute file paths
 filepath = os.path.abspath(filename)
 filebase = os.path.basename(filename)
 fileext = filename.split('.')[-1]
+
 print(f"Converting from {fileext} to {frm}")
+
+#only run if converting to a different format
 if not(filename.endswith(str("." + frm))):
     track = AudioSegment.from_file(filename, fileext)
     newname = filebase.replace(fileext, frm)
+
+    #save to the same folder as original file
     newpath = filepath.replace(filebase, newname)
     track.export(newpath, format=frm)
     print(f"File saved to {newpath}")
