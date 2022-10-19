4ddd8f59b87334b4e82ae4acae436b57b2437910
Targgus <jhenson@Johns-MacBook-Air.local>
2022-10-18 18:34:34 -0600

    removed DRY code and added error handling. also added improved functionality

--- projects/Guess Number/guess_number.py
+++ projects/Guess Number/guess_number.py
@@ -1,18 +1,41 @@
 import random
 
+smaller_number = 1
+larger_number = 10
+
+def prompt_user():
+    while True:
+        try:
+            user_guess = int(input(f"\nEnter number between 1 to {larger_number}: "))
+            return user_guess
+        except Exception as e:
+            print("\nThat guess was invalid. Try again.")
+    
 
 def guess_number(user_guess):
-    num = random.randint(1, 50)
+    num = random.randint(smaller_number, larger_number)
     while num != user_guess:
-        if num < user_guess:
-            print("Number is lower than " + str(user_guess))
-            user_guess = int(input("Enter number between 1 to 50: "))
+        if user_guess > larger_number:
+            print(f"\nYour guess exceeds the upper range. Lower your guess and try again.")
+            user_guess = prompt_user()
+        elif user_guess < smaller_number:
+            print(f"\nYour guess is exceeds the lower range. Increase your guess and try again.")
+            user_guess = prompt_user()
+        elif num < user_guess:
+            print(f"\nNumber is lower than {user_guess}")
+            user_guess = prompt_user()
         elif num > user_guess:
-            print("Number is higher than " + str(user_guess))
-            user_guess = int(input("Enter number between 1 to 50: "))
+            print(f"\nNumber is higher than {user_guess}")
+            user_guess = prompt_user()
         else:
             print()
+    print(f"\nCongrats! You've guessed the correct number! It was {num}.\n")
 
-
-user_guess = int(input("Enter number between 1 to 50: "))
-guess_number(user_guess)
+while True:
+    play_y_n = input("Welcome to Number Guesser. If you'd like to play, press 'Y': ")
+    if play_y_n.lower() == 'y':
+        user_guess = prompt_user()
+        guess_number(user_guess)
+    else:
+        print("Thanks for playing!")
+        break
