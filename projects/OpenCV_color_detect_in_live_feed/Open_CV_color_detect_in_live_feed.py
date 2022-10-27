675ab1964ec7828cf773083268003ff74eaa35fa
Lint Action <lint-action@samuelmeuli.com>
2022-10-27 05:41:43 +0000

    Fix code style issues with Black

--- projects/OpenCV_color_detect_in_live_feed/Open_CV_color_detect_in_live_feed.py
+++ projects/OpenCV_color_detect_in_live_feed/Open_CV_color_detect_in_live_feed.py
@@ -1,25 +1,31 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0) #This allows the opencv to access your webcam, you can change it from 0 to any integer 1 or 2 if you are using another webcam or camera
-cap.set(3,1080)
-cap.set(4,720)
-cap.set(10,100)
+cap = cv2.VideoCapture(
+    0
+)  # This allows the opencv to access your webcam, you can change it from 0 to any integer 1 or 2 if you are using another webcam or camera
+cap.set(3, 1080)
+cap.set(4, 720)
+cap.set(10, 100)
 
-while True: # we are using a loop, because we get a stream of images called frames and by using loop we can convert them into continuous stream i.e. video
-    success, img = cap.read()  
-    hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV) #converting the image captured from rgb format to hsv format as it is easier to do color detection on it
-    height,width, _ = img.shape
+while (
+    True
+):  # we are using a loop, because we get a stream of images called frames and by using loop we can convert them into continuous stream i.e. video
+    success, img = cap.read()
+    hsv_img = cv2.cvtColor(
+        img, cv2.COLOR_BGR2HSV
+    )  # converting the image captured from rgb format to hsv format as it is easier to do color detection on it
+    height, width, _ = img.shape
 
-    cx =int(width/2)
-    cy = int(height/2)
-    cv2.circle(img,(cx,cy),10,(0,255,0),2)
+    cx = int(width / 2)
+    cy = int(height / 2)
+    cv2.circle(img, (cx, cy), 10, (0, 255, 0), 2)
 
-    pixel_center_bgr = img[cy,cx]
+    pixel_center_bgr = img[cy, cx]
     pixel_center = hsv_img[cy, cx]
     hue_val = pixel_center[0]
 
-    color = "Undefined" #below loops gives an output of detected color based on the hue value range
+    color = "Undefined"  # below loops gives an output of detected color based on the hue value range
     if hue_val < 5:
         color = "RED"
     elif hue_val < 22:
@@ -36,14 +42,20 @@ while True: # we are using a loop, because we get a stream of images called fram
         color = "BLACK"
     else:
         color = "RED"
-    
-    b, g, r = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2]) #3 lines here determine the UI element of cv2 frame
+
+    b, g, r = (
+        int(pixel_center_bgr[0]),
+        int(pixel_center_bgr[1]),
+        int(pixel_center_bgr[2]),
+    )  # 3 lines here determine the UI element of cv2 frame
     cv2.rectangle(img, (cx - 220, 10), (cx + 200, 120), (255, 255, 255), -1)
     cv2.putText(img, color, (cx - 200, 100), 0, 3, (b, g, r), 5)
 
     cv2.imshow("Video", img)
-    if cv2.waitKey(1)& 0xFF == ord('q'): #this lines tells opencv to exit out of while loop when q is pressed
+    if cv2.waitKey(1) & 0xFF == ord(
+        "q"
+    ):  # this lines tells opencv to exit out of while loop when q is pressed
         break
 
-cap.release() 
+cap.release()
 cv2.destroyAllWindows()
