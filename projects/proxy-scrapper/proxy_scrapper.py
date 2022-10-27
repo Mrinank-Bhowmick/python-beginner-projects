dee50ecf70b6b666940ee41ceef67f6bedf886b7
Lint Action <lint-action@samuelmeuli.com>
2022-10-27 06:57:50 +0000

    Fix code style issues with Black

--- projects/proxy-scrapper/proxy_scrapper.py
+++ projects/proxy-scrapper/proxy_scrapper.py
@@ -5,15 +5,15 @@ url = "https://free-proxy-list.net/"
 
 data = requests.get(url).text
 
-soup = BeautifulSoup(data, 'html.parser')
+soup = BeautifulSoup(data, "html.parser")
 
-table = soup.find('table', class_='table')
+table = soup.find("table", class_="table")
 
-for row in table.tbody.find_all('tr'):    
-    columns = row.find_all('td')
-    
-    if(columns != []):
+for row in table.tbody.find_all("tr"):
+    columns = row.find_all("td")
+
+    if columns != []:
         ip = columns[0].text.strip()
         port = columns[1].text.strip()
-        with open('proxies.txt', 'a') as f:
+        with open("proxies.txt", "a") as f:
             f.write(f"{ip}:{port}\n")
