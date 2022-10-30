5b0cc86e3bf36caa0d0356f2b030ed11f733f81f
Lint Action <lint-action@samuelmeuli.com>
2022-10-30 12:46:49 +0000

    Fix code style issues with Black

--- projects/Image Toonification/setup.py
+++ projects/Image Toonification/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name = "QRCode-Scanner",
-    version = '1.0',
-    author = "Logadheep", 
-    author_email = "logadheep3438@gmail.com",
-    description = "QRcode Scanning app using python"
+    name="QRCode-Scanner",
+    version="1.0",
+    author="Logadheep",
+    author_email="logadheep3438@gmail.com",
+    description="QRcode Scanning app using python",
 )
--- projects/Rubik-tracking/rubik-tracking.py
+++ projects/Rubik-tracking/rubik-tracking.py
@@ -18,43 +18,43 @@ vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
 while 1:
-	frame = vs.read()
+    frame = vs.read()
 
-	frame = imutils.resize(frame, width=700)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    frame = imutils.resize(frame, width=700)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
 
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-	center = None
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
 
-	if len(cnts) > 0:
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-		if radius > 10:
-			cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
+        if radius > 10:
+            cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
 
-	pts.appendleft(center)
+    pts.appendleft(center)
 
-	for i in range(1, len(pts)):
-		if pts[i - 1] is None or pts[i] is None:
-			continue
+    for i in range(1, len(pts)):
+        if pts[i - 1] is None or pts[i] is None:
+            continue
 
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 4.5)
-		cv2.line(frame, pts[i - 1], pts[i], (0, 255, 250), thickness)
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 4.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 255, 250), thickness)
 
-	cv2.imshow("Rubik's cube tracking", frame)
-	key = cv2.waitKey(1) & 0xFF
+    cv2.imshow("Rubik's cube tracking", frame)
+    key = cv2.waitKey(1) & 0xFF
 
-	if key == ord("q"):
-		break
+    if key == ord("q"):
+        break
 
 vs.release()
 cv2.destroyAllWindows()
