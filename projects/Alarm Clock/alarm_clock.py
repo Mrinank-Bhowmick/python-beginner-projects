cb1cc7fe1209175a68a821e620afac1665755cbf
Lint Action <lint-action@samuelmeuli.com>
2022-10-30 12:50:18 +0000

    Fix code style issues with Black

--- projects/Alarm Clock/alarm_clock.py
+++ projects/Alarm Clock/alarm_clock.py
@@ -23,8 +23,11 @@ def alarm():
 
         if current_time == set_alarm_time:
             print("Wake Up now!")
-            #play sound continuously
-            winsound.PlaySound("sound.wav", winsound.SND_ASYNC|winsound.SND_LOOP|winsound.SND_FILENAME)
+            # play sound continuously
+            winsound.PlaySound(
+                "sound.wav",
+                winsound.SND_ASYNC | winsound.SND_LOOP | winsound.SND_FILENAME,
+            )
 
 
 Label(root, text="Alarm Clock", font=("Helvetica 20 bold"), fg="red").pack(pady=10)
@@ -207,6 +210,8 @@ secs.pack(side=LEFT)
 Button(root, text="Set Alarm", font=("Helvetica 15"), command=Threading).pack(pady=20)
 
 stop_alarm = lambda: winsound.PlaySound(None, winsound.SND_PURGE)
-button = Button(root, text='Stop Alarm', bg='red',fg='white', command=stop_alarm).pack(pady=30)
+button = Button(root, text="Stop Alarm", bg="red", fg="white", command=stop_alarm).pack(
+    pady=30
+)
 
 root.mainloop()
