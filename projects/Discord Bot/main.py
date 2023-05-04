b1d711cccf0096523c11c15038c3d4136b99127e
Lint Action <lint-action@samuelmeuli.com>
2023-05-04 14:38:13 +0000

    Fix code style issues with Black

--- projects/Discord Bot/main.py
+++ projects/Discord Bot/main.py
@@ -6,22 +6,24 @@ import random
 
 token = "Your Token Here"
 
-bot=discord.bot(comand_prefix='!')
+bot = discord.bot(comand_prefix="!")
+
 
 @bot.event
 async def on_ready():
     print("Bot is ready")
-    
+
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-    
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
-        
-    if message.content.startswith('!random'):
-        await message.channel.send(random.randint(1,100))
+
+    if message.content.startswith("!hello"):
+        await message.channel.send("Hello!")
+
+    if message.content.startswith("!random"):
+        await message.channel.send(random.randint(1, 100))
+
 
 bot.run(token)
-
