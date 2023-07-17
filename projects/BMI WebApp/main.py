4ce4288cb0bc567c7fc76818ad3573b5de663fbe
Ota Hina <108225969+otahina@users.noreply.github.com>
2023-07-17 11:08:23 +0900

    Update main.py

--- projects/BMI WebApp/main.py
+++ projects/BMI WebApp/main.py
@@ -3,21 +3,6 @@ from pywebio.output import *
 
 
 class calculation:
-    def BMIcalculator(Height, Mass):
-        for t1, t2 in [
-            (16, "severely underweight"),
-            (18.5, "underweight"),
-            (25, "normal"),
-            (30, "overweight"),
-            (35, "moderately obese"),
-            (float("inf"), "severely obese"),
-        ]:
-            if BMI <= t1:
-                put_text("Your BMI is", BMI, "and the person is :", t2)
-                break
-
-
-class calculation:
     def BMIcalculator(self, Height, Mass):
         BMI = (Mass) / (Height * Height)
         for t1, t2 in [
