d4d7e4786dbca3977128d581a35e283332df68a0
Khushhal Garg <khusgarg1@gmail.com>
2023-07-19 13:28:27 +0530

    Comment added

--- projects/Data Entry Automation/main.py
+++ projects/Data Entry Automation/main.py
@@ -4,14 +4,17 @@ import requests
 from selenium.webdriver.common.by import By
 import time
 
-
+# Your Chrome Path
 chrome_path = "YOUR_CHROME_DRIVER_PATH"
 
+# Your Google Form Link
 FORM = "YOUR_GOOGLE_FORM"
+# Three empty list to save Scraped data
 ADDRESS = []
 PRICE = []
 LINK = []
 
+# Header to add with requests.get
 HEADERS = {
     "User-Agent": "YOUR_USER_AGENT",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
@@ -21,7 +24,7 @@ HEADERS = {
 class HouseData:
     def __init__(self):
         self.driver = webdriver.Chrome(executable_path=chrome_path)
-
+        # Making a request to the website 
         self.web = requests.get(
             "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D",
             headers=HEADERS,
@@ -30,6 +33,22 @@ class HouseData:
 
         self.soup = BeautifulSoup(self.my_html, "html.parser")
 
+    # This function help in selection the specific elemen(address,link)t from the web page
+    def my_css(self, css_class):
+        return (
+            css_class is not None
+            and "StyledPropertyCardDataArea-c11n-8-89-0__sc-yipmu-0 gZUDVm property-card-link"
+            in css_class
+        )
+     # This function help in selection the specific elemen(price)t from the web page
+    def my_price(self, css_class):
+        return (
+            css_class is not None
+            and "PropertyCardWrapper__StyledPriceLine-srp__sc-16e8gqd-1 iMKTKr"
+            in css_class
+        )
+    
+    # This Function find data we need and add them to the lists
     def scrape(self):
         # print(self.soup.prettify())
         address = self.soup.find_all(class_=self.my_css)
@@ -40,21 +59,8 @@ class HouseData:
 
         for j in price:
             PRICE.append(j.text)
-            
-    def my_css(self, css_class):
-        return (
-            css_class is not None
-            and "StyledPropertyCardDataArea-c11n-8-89-0__sc-yipmu-0 gZUDVm property-card-link"
-            in css_class
-        )
-
-    def my_price(self, css_class):
-        return (
-            css_class is not None
-            and "PropertyCardWrapper__StyledPriceLine-srp__sc-16e8gqd-1 iMKTKr"
-            in css_class
-        )
-
+    
+    # This function use selenium and fill form with the data we scrape
     def form(self):
         for i in range(len(ADDRESS)):
             self.driver.get(FORM)
@@ -83,7 +89,7 @@ class HouseData:
             )
             submit.click()
 
-
+# Making object of our class and calling its function
 bot = HouseData()
 bot.scrape()
 bot.form()
