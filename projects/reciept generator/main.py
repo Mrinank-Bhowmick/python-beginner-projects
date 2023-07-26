6f0b8343c9e103fcb65215bcaa640cd08a820144
Lint Action <lint-action@samuelmeuli.com>
2023-07-26 06:15:21 +0000

    Fix code style issues with Black

--- projects/reciept generator/main.py
+++ projects/reciept generator/main.py
@@ -30,34 +30,36 @@ i = 1
 # Begin a loop to collect product information and calculate the bill
 while True:
     # Prompt the user to enter the product name or 'q' to print the final bill
-    productPurchased = input("Enter the product name (Press q to print the final bill): \n")
-    
+    productPurchased = input(
+        "Enter the product name (Press q to print the final bill): \n"
+    )
+
     # If the user enters 'q', break the loop and proceed to printing the bill
     if productPurchased == "q":
         break
-    
+
     # Prompt the user to enter the product quantity
     productQuantity = int(input("Enter the product quantity : \n"))
-    
+
     # Prompt the user to enter the product code
     productCode = input("Enter the product code : \n")
-    
+
     # Prompt the user to enter the unit price of the product
     productPrice = input("Enter the unit price : \n")
-    
+
     # If the user did not enter 'q', calculate the total cost of the product and add it to the overall sum
     if productPurchased != "q":
         sum = sum + (float(productPrice) * productQuantity)
-        
+
         # Create a string to represent the billing information for the current product
         billing = f"{i}). \t {productPurchased} of code {productCode}, quantity of {productQuantity} \n billed  {productPrice}x{productQuantity} units  =  \n \t Rs. {(float(productPrice))*productQuantity}  "
-        
+
         # Write the billing information to the receipt text file
         f.write(billing + "\n")
-        
+
         # Print the billing information to the console
         print(billing)
-        
+
         # Increment the counter for the next product
         i += 1
 
@@ -65,7 +67,9 @@ while True:
 print(f"order total so far Rs. {sum}")
 
 # Create a string to thank the customer for shopping and display the total bill amount
-thanq = f"\n\n!!! Your bill total is only Rs. {sum}. Thanks for shopping with us !!!\n\n"
+thanq = (
+    f"\n\n!!! Your bill total is only Rs. {sum}. Thanks for shopping with us !!!\n\n"
+)
 
 # Write the thank-you message to the receipt text file
 f.write(thanq)
@@ -91,4 +95,3 @@ pdf.output("billGenerated.pdf")
 
 # Close the receipt text file
 f.close()
-
