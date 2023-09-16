29ed33eeebb854714b3bab65ead9fbc14d91dfb5
Dhandeep <106858770+Dhandeep10@users.noreply.github.com>
2023-09-17 00:05:52 +0530

    Added new features and game has been improved
    Following changes has been made:
    
    1. Multiple Overs: Allow the game to be played with multiple overs (e.g., 1 over, 5 overs, 10 overs), and display the current over and remaining balls.
    
    2. User and Computer Batting Order: Implemented a feature that allows users to choose whether the user want to bat or bowl first. The user and computer will take turns batting and bowling.
    
    3. Difficulty Levels: Added difficulty levels (Easy, Medium, Hard) to the computer's batting and bowling skills. Adjust the computer's choices based on the selected difficulty level.
    
    4. Score Tracking: Keep track of the total runs scored by both the user and the computer. 
    
    5. Wickets: Introduce the concept of wickets. The user and computer will have a limited number of wickets (e.g., 10 wickets each), and the game ends when all wickets are lost.
    
    6. Alternate Batting: After each over, the batting and bowling roles alternate between the user and the computer.
    
    7. Enhanced UI: Improved the user interface by displaying a scoreboard of each ball's result

--- projects/HandCricket/main.py
+++ projects/HandCricket/main.py
@@ -1,117 +1,115 @@
-# Terminal based hand cricket game
-
 import random
 import time
 
-
 def main():
-    print("Welcome to Hand Cricket")
+    print("Welcome Hand Cricket")
     print("You will be playing against the computer")
-    print("Enter 1 to bat first, 2 to bowl first")
     try:
-        choice = int(input())
-        if choice == 1:
-            our_score = bat()
-            computers_score = bowl()
-            who_won(our_score, computers_score)
+        overs = int(input("Enter the number of overs (1-10): "))
+        user_choice = input("Enter 1 to bat first, 2 to bowl first: ")
+        difficulty = int(input("Select difficulty level (1-Easy, 2-Medium, 3-Hard): "))
+        
+        user_score, computer_score = play_game(overs, user_choice, difficulty)
+        
+        who_won(user_score, computer_score)
+    except ValueError:
+        print("Invalid input, exiting game")
 
-        elif choice == 2:
-            computers_score = bowl()
-            our_score = bat()
-            who_won(our_score, computers_score)
+def play_game(overs, user_choice, difficulty=1):
+    user_score = 0
+    computer_score = 0
+    user_wickets = 10
+    computer_wickets = 10
+    
+    print("\nMatch Summary")
+    print("=============")
+    print(f"Overs: {overs}")
+    
+    for over in range(overs):
+        print(f"\nOver {over + 1}, User: {user_wickets} wickets left, Computer: {computer_wickets} wickets left")
+        
+        if user_choice == '1':
+            user_score, user_wickets = user_bat(user_score, user_wickets, over)
+            
+            if computer_wickets > 0:
+                computer_score, computer_wickets = computer_bat(computer_score, computer_wickets, difficulty, over)
         else:
-            print("Invalid choice")
-            main()
-    except Exception as e:
-        print("Invalid choice,exiting game")
-        print(e)
+            if computer_wickets > 0:
+                computer_score, computer_wickets = computer_bat(computer_score, computer_wickets, difficulty, over)
+                
+            user_score, user_wickets = user_bat(user_score, user_wickets, over)
+        
+        display_scoreboard(user_score, computer_score, over)
 
+    return user_score, computer_score
 
-def who_won(our_score, computers_score):
-    print()
-    print("Match Result")
-    print("===========")
-    print("You scored=", our_score)
-    print("Computer scored=", computers_score)
-    if our_score > computers_score:
+def user_bat(user_score, user_wickets, over):
+    print("You are batting")
+    balls = 0
+    while balls < 6 and user_wickets > 0:
+        user_runs = int(input(f"Over {over + 1}, Ball {balls + 1}: Enter your shot (1-6): "))
+        computer_runs = random.randint(1, 6)
+    
+        print(f"You chose {user_runs}, Computer chose {computer_runs}")
+    
+        if user_runs == computer_runs:
+            print("You are out!")
+            user_wickets -= 1
+            if user_wickets > 0:
+                print(f"You have {user_wickets} wickets left.")
+        else:
+            user_score += user_runs
+            print(f"Your score is {user_score}")
+        balls += 1
+    
+    return user_score, user_wickets
+
+def computer_bat(computer_score, computer_wickets, difficulty=1, over=None):
+    print("Computer is batting")
+    balls = 0
+    while balls < 6 and computer_wickets > 0:
+        if difficulty == 1:
+            computer_runs = random.randint(1, 3)
+        elif difficulty == 2:
+            computer_runs = random.randint(1, 5)
+        else:
+            computer_runs = random.randint(1, 6)
+    
+        user_runs = int(input(f"Computer is batting. Over {over + 1}, Ball {balls + 1}: Enter your delivery (1-6): "))
+    
+        print(f"You chose {user_runs}, Computer chose {computer_runs}")
+    
+        if user_runs == computer_runs:
+            print("Computer is out!")
+            computer_wickets -= 1
+            if computer_wickets > 0:
+                print(f"Computer has {computer_wickets} wickets left.")
+        else:
+            computer_score += computer_runs
+            print(f"Computer's score is {computer_score}")
+        balls += 1
+    
+    return computer_score, computer_wickets
+
+def display_scoreboard(user_score, computer_score, over):
+    print("\nScoreboard")
+    print("==========")
+    print(f"Over {over + 1}:")
+    print(f"You: {user_score} runs")
+    print(f"Computer: {computer_score} runs")
+
+def who_won(user_score, computer_score):
+    print("\nMatch Result")
+    print("============")
+    print("Your score =", user_score)
+    print("Computer's score =", computer_score)
+    if user_score > computer_score:
         print("You won")
-    elif computers_score > our_score:
+    elif computer_score > user_score:
         print("You lost")
     else:
-        print("You Drew")
+        print("The match ended in a draw")
     print("Thank you for playing and have a good day :) ")
-
-
-def bat():
-    print()
-    print("You are batting")
-    print("Rules")
-    print("=====")
-    print("You need to chose a number and the computer chooses a number")
-    print("If they are the same you are out")
-    print("If they are different you score some runs")
-    print()
-    print("Game start")
-    print("=========")
-    score = 0
-    while True:
-        print("Enter a number between 1 and 6 or Enter 0 to quit")
-        user = int(input())
-        if user == 0:
-            print("Your score is", score)
-            break
-        elif user < 1 or user > 6:
-            print("Invalid input")
-            continue
-        else:
-            comp = random.randint(1, 6)
-            print("Computer chose", comp)
-            if user == comp:
-                print()
-                print("You are out")
-                print("Your score is", score)
-                input("press enter to continue")
-                break
-            else:
-                score += user
-                print("Your score is", score)
-    return score
-
-
-def bowl():
-    print("You are bowling")
-    print("Rules")
-    print("=====")
-    print("You need to chose a number and the computer chooses a number")
-    print("If they are the same you bowl the computer out")
-    print("If they are different the computer scores runs")
-    print()
-    print("Game start")
-    print("=========")
-    score = 0
-    while True:
-        print("Enter a number between 1 and 6 or Enter to quit")
-        user = int(input())
-        comp = random.randint(1, 6)
-        print("Computer chose", comp)
-        if user == 0:
-            print("Computer score is", score)
-            break
-        elif user < 1 or user > 6:
-            print("Invalid input")
-            continue
-        else:
-            if user == comp:
-                print()
-                print("Computer is out")
-                print("Computer score is", score)
-                input("press enter to continue")
-                break
-            else:
-                score += comp
-                print("Computer score is", score)
-    return score
-
 
 if __name__ == "__main__":
     main()
