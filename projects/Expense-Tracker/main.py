34cfd7c5000c8d59d761de1b785892efc66b57d2
Anish Vantagodi <anishvv212002@gmail.com>
2023-10-03 07:44:55 +0530

    Data filter feature is added

--- projects/Expense-Tracker/main.py
+++ projects/Expense-Tracker/main.py
@@ -1,15 +1,10 @@
 import sqlite3
-import csv 
+import csv
 
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
-from reportlab.lib.styles import getSampleStyleSheet
 # Connect to the database
 conn = sqlite3.connect("expenses.db")
 cursor = conn.cursor()
 
-# Define SQL statements
 create_table_sql = """CREATE TABLE IF NOT EXISTS expenses (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, description TEXT, amount REAL)"""
 insert_expense_sql = "INSERT INTO expenses (date, description, amount) VALUES (?, ?, ?)"
 select_expenses_sql = "SELECT * FROM expenses ORDER BY date;"
@@ -24,12 +19,15 @@ if not conn.execute(
     conn.commit()
 
 
+# Define SQL statements
+
+
 def add_expense():
     global cursor
 
     # Get input from user
     print("Enter date (YYYY-MM-DD): ")
-    date = input().split()[0] 
+    date = input().split()[0]
     print("Enter description: ")
     description = input()
     print("Enter amount: ")
@@ -45,34 +43,38 @@ def add_expense():
 
 def delete_expense():
     global cursor
+    if is_empty() == True:
+        print("No expenses recorded yet.")
+    else:
+        # Get ID from user
+        print("Enter ID to delete: ")
+        id = int(input())
 
-    # Get ID from user
-    print("Enter ID to delete: ")
-    id = int(input())
-
-    try:
-        cursor.execute(delete_expense_by_id_sql, (id,))
-        conn.commit()
-        print("Expense deleted successfully.")
-    except Exception as e:
-        print("Error deleting expense: {}".format(e))
+        try:
+            cursor.execute(delete_expense_by_id_sql, (id,))
+            conn.commit()
+            print("Expense deleted successfully.")
+        except Exception as e:
+            print("Error deleting expense: {}".format(e))
 
 
 def update_expense():
     global cursor
+    if is_empty() == True:
+        print("No expenses recorded yet.")
+    else:
+        # Get ID and new description from user
+        print("Enter ID to update: ")
+        id = int(input())
+        print("Enter new description: ")
+        description = input()
 
-    # Get ID and new description from user
-    print("Enter ID to update: ")
-    id = int(input())
-    print("Enter new description: ")
-    description = input()
-
-    try:
-        cursor.execute(update_expense_by_id_sql, (description, id))
-        conn.commit()
-        print("Expense updated successfully.")
-    except Exception as e:
-        print("Error updating expense: {}".format(e))
+        try:
+            cursor.execute(update_expense_by_id_sql, (description, id))
+            conn.commit()
+            print("Expense updated successfully.")
+        except Exception as e:
+            print("Error updating expense: {}".format(e))
 
 
 def view_expenses():
@@ -93,6 +95,26 @@ def total_expenses():
     total = conn.execute("SELECT SUM(amount) FROM expenses;").fetchone()[0]
     print(f"Total expenses: {total}$")
 
+
+# defining function for exporting to csv
+def print_csv():
+    # opening csv file
+    with open("expense.csv", "w") as csvfile:
+        #
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(
+            ["ID", "DATE", "Desc", "description"]
+        )  # To Define heading of rows of csv
+        csvwriter.writerows(cursor)
+        expenses = conn.execute(select_expenses_sql).fetchall()
+        for i in expenses:
+            print(i)
+            csvwriter.writerow(i)  # writing querys to csv
+        total = conn.execute("SELECT SUM(amount) FROM expenses;").fetchone()[
+            0
+        ]  # For total Expenses
+        csvwriter.writerow(["", "", "Total Expenses:", total])  # Printing to CSV
+        
 def data_filter():
     print("Select Filter Option:")
     print("1. Filter by Date")
@@ -155,66 +177,16 @@ def data_filter():
             )
 
 
-
-
-def convert_to_excel():
-    # Convert expense data from database to excel
-    
-    # Define SQL query to select all data from 'expenses' table
-    query = "Select * from expenses"
-    
-    # Execute the query and fetch data
-    cursor.execute(query)
-    data = cursor.fetchall()
-    
-    # Add column headers to the data
-    data = [('Id', 'Date', 'Description', 'Amount')] + data
-        
-    # Write data to a CSV file named 'expenses.csv'
-    with open('expenses.csv', 'w', newline='') as file:
-        csv_writer = csv.writer(file)
-        csv_writer.writerows(data)
-        
-def convert_to_pdf():
-    # Convert expense data from database to PDF
-    
-    # Define SQL query to select all data from 'expenses' table
-    query = "SELECT * FROM expenses"
-    
-    # Execute the query and fetch data
-    cursor.execute(query)
-    data = cursor.fetchall()
-    
-    # Add column headers to the data
-    data = [('Id', 'Date', 'Description', 'Amount')] + data
-
-    # Define PDF filename as 'expenses.pdf' and set page size to letter
-    pdf_filename = 'expenses.pdf'
-    pdf = SimpleDocTemplate(pdf_filename, pagesize=letter)
-    
-    # Create a table from the data
-    table = Table(data)
-    
-    # Define style for the table
-    style = TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-                        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-                        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-                        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-                        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-                        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-                        ('GRID', (0, 0), (-1, -1), 1, colors.black)])
-    table.setStyle(style)
-    
-    # Define a title for the PDF
-    styles = getSampleStyleSheet()
-    title = Paragraph("<b>Expenses</b>", styles['Title'])
-
-    # Build the PDF with title and table
-    pdf.build([title, table])
-
+# defined this funtion to check is database is empty or not
+def is_empty():
+    if conn.execute(select_expenses_sql).fetchall() == []:
+        return True
+    else:
+        return False
 
 
 def main_menu():
+    # conn=db_init()
     while True:
         print("\nExpense Tracker Menu:")
         print("1. Add Expense")
@@ -222,12 +194,11 @@ def main_menu():
         print("3. Total Expenses")
         print("4. Delete Expense")
         print("5. Update Expense Description")
-        print("6. Data Export to Excel")
-        print("7. Data Export to PDF")
-        print("8. Data Filter")
-        print("9. Quit")
+        print("6. Export Expense")
+        print("7. Data Filter")
+        print("8. Quit")
 
-        choice = input("Enter your choice (1-9): ")
+        choice = input("Enter your choice (1-8): ")
 
         if choice == "1":
             add_expense()
@@ -240,18 +211,17 @@ def main_menu():
         elif choice == "5":
             update_expense()
         elif choice == "6":
-            convert_to_excel()
+            print_csv()
         elif choice == "7":
-            convert_to_pdf()
-        elif choice == "8":
             data_filter()
-        elif choice == "9":
-            break
+        elif choice == "8":
+            is_empty()
         else:
             print("Invalid choice. Please try again.")
 
 
 if __name__ == "__main__":
     main_menu()
+
 
 conn.close()
