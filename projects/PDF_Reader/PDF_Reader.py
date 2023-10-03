0c9061436d13628a04bb75460b711c8f33490d80
Lint Action <lint-action@samuelmeuli.com>
2023-10-03 19:18:06 +0000

    Fix code style issues with Black

--- projects/PDF_Reader/PDF_Reader.py
+++ projects/PDF_Reader/PDF_Reader.py
@@ -14,7 +14,7 @@ from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
 openai_api_key = os.getenv("API_KEY")
-openai.openai_api_key=openai_api_key
+openai.openai_api_key = openai_api_key
 
 loader = PyPDFLoader("promptEngineering.pdf")
 docs = loader.load_and_split()
@@ -25,36 +25,44 @@ embeddings = OpenAIEmbeddings()
 vectorstore = Chroma.from_documents(documents, embeddings)
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
+
 def originalText(docs):
-    text=str(docs)
+    text = str(docs)
     regex = r"(Document)|(page_content=)|(metadata={'source':)|('page': \d})\)|(\\n)"
-    text=re.sub(regex, "", text)
+    text = re.sub(regex, "", text)
     return text
 
-text=originalText(docs)
+
+text = originalText(docs)
 print("Text in pdf", text)
+
 
 def summarizeText():
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
     chain = load_summarize_chain(llm, chain_type="stuff")
     return chain.run(docs)
 
-summary=summarizeText()
+
+summary = summarizeText()
 print("Summary of text in pdf", summary)
 
+
 def QA(query):
-    qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0), vectorstore.as_retriever(), memory=memory)
+    qa = ConversationalRetrievalChain.from_llm(
+        OpenAI(temperature=0), vectorstore.as_retriever(), memory=memory
+    )
     query = "What is prompt engineering?"
     result = qa({"question": query})
-    result=str(result['chat_history'][1])
-    result=result.split('content=\'')[1]
+    result = str(result["chat_history"][1])
+    result = result.split("content='")[1]
     return result
 
+
 print("INSTRUCTIONS:")
-print("Enter the question you want to ask from pdf text OR press \"-1\" to STOP")
-while(True):
-    user_input=input("Enter your question: ")
-    if(user_input=="-1"):
+print('Enter the question you want to ask from pdf text OR press "-1" to STOP')
+while True:
+    user_input = input("Enter your question: ")
+    if user_input == "-1":
         break
     else:
         print(QA(user_input))
