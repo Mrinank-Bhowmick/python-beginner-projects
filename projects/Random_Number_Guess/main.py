5a7b0bb81368e28bee44c40555f1c7182b8a36c8
Sanyam Jain <sanyamjain6667@gmail.com>
2023-10-11 15:22:15 +0530

    Add files via upload

---
+++ projects/Random_Number_Guess/main.py
@@ -0,0 +1,27 @@
+import random
+randNumber = random.randint(1, 100)
+userGuess = None
+guesses = 0
+
+while(userGuess != randNumber):
+    userGuess = int(input("Enter your guess: "))
+    guesses += 1
+    if(userGuess==randNumber):
+        print("You guessed it right!")
+    else:
+        if(userGuess>randNumber):
+            print("You guessed it wrong! Enter a smaller number")
+        else:
+            print("You guessed it wrong! Enter a larger number")
+
+print(f"You guessed the number in {guesses} guesses")
+with open("hiscore.txt", "r") as f:
+    hiscore = int(f.read())
+
+if(guesses<hiscore):
+    print("You have just broken the high score!")
+    with open("hiscore.txt", "w") as f:
+        f.write(str(guesses))
+
+
+        
