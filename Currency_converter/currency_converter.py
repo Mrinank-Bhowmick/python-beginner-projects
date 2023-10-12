8050a4e54e6a3e81a0325c6a4babd95318e0678c
Spandan Tripathy <128175097+CrypticRevenger@users.noreply.github.com>
2023-10-12 12:16:07 +0530

    Currency Converter

---
+++ Currency_converter/currency_converter.py
@@ -0,0 +1,13 @@
+with open('currencyData.txt') as f:
+	lines = f.readlines()
+
+currencyDict = {}
+for line in lines:
+	parsed = line.split("\t")
+	currencyDict[parsed[0]] = parsed[1]
+
+amount = int(input("Enter amount:\n"))
+print("Enter the name of the currency you want to convert this amount to? Available Options:\n")
+[print(item) for item in currencyDict.keys()]
+currency = input("Please enter one of these values: \n")
+print(f"{amount} INR is equal to {amount *float(currencyDict[currency])} {currency}")
