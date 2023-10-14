059107fb35070f21a43c05ce516e4389809a23fe
nikcode9 <129772745+nikcode9@users.noreply.github.com>
2023-10-15 00:19:26 +0530

    Create pygame.py

---
+++ pygame.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((400, 300))
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill((255, 255, 255))
+    pygame.display.flip()
+
+pygame.quit()
