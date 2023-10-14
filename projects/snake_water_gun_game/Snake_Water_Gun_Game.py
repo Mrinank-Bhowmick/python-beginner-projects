c1f84e741e517f0387b3d6f4cfc9d8deed459e57
Abhay Vishwakarma <133063358+abhayv290@users.noreply.github.com>
2023-10-15 05:28:48 +0530

    Update Snake_Water_Gun_Game.py some usefull comment to understandable code

--- projects/snake_water_gun_game/Snake_Water_Gun_Game.py
+++ projects/snake_water_gun_game/Snake_Water_Gun_Game.py
@@ -1,15 +1,21 @@
-import random  #importing the random keyint for guessing by computer feature WE Can use two input method too if you wanna play with a teammate
+import random
 
 comp= random.randint(0,2)
 # print(comp)
 
-# points counting
-#
+#Using input to take the input from the user
+# 0 for snake 1 for water and 2 for gun 
+#as rule defined snake drinks water ,water damaged gun,and gun kills snake 
 
+# points counting 
 comp_score=0    
-user_score=0    
-for i in range(10):   
+user_score=0
+n_times=0   
+while(True):
+    if(n_times==5):
+     break   
     user_inp=int(input("Enter your prefered number (0 for snake 1 for water and 2 for Gun and 5 for quit)"))
+    n_times=user_inp
     print(f"computer's turn {comp}")
     if(comp==user_inp):
         print('Game draw!')
@@ -35,7 +41,7 @@ for i in range(10):
          
         
 
-#Uses ternary operators for this line two reduce the codebase
+
 print('User Won with the score',user_score) if (user_score>comp_score) else print('computer won with the score',comp_score)
     
 
