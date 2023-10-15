9fe2595258dd0ef6432faa305df0d3f7f12fb3cb
aursh_sinha <121930064+aursh24@users.noreply.github.com>
2023-10-15 10:58:09 +0530

    Multiplication Table
    I have added a python program which will display the Mathematical Table for any given number.

--- projects/Table.py
+++ projects/Table.py
@@ -1,10 +1,10 @@
 def table():
-    while True:
-        n=int(input('Enter the Number : '))
-        for i in range(1,11):
-            print(n,'x',i,'=',n*i)
+    while True:                             #will stay True forever
+        n=int(input('Enter the Number : '))   #user input
+        for i in range(1,11):               #loop running from 1 to 10, 11 is exclusive
+            print(n,'x',i,'=',n*i)  
         print('\n')    
-        print('Want to Know Table of any Other Number?')
+        print('Want to Know Table of any Other Number?')    #restarting the loop from the start, as per the user choice
         print('Press (y/n)')
         ch=input()
         if ch=='N' or ch=='n':
