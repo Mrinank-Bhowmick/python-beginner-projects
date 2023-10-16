ba47d29ae3845738c0ddba151e8ab51a36683ebd
Santhosh S <108118100+Santhoshnov@users.noreply.github.com>
2023-10-16 06:15:16 +0530

    Create gpt3.py

---
+++ projects/ImagegenChatbot/gpt3.py
@@ -0,0 +1,20 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def response(query):
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt = query,
+      temperature=0,
+      max_tokens=1000,
+    )
+    data = response['choices'][0]['text']
+    return data
+
+while True:
+    query = input(">> ")
+    if query == "bye":
+        break
+    print(response(query))
