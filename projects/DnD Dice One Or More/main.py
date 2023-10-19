269f820a50e74af437e5d6eb23b9a23d6f63b329
Esther <esther.debaecke@student.hogent.be>
2023-10-20 00:33:49 +0200

    My first Hacktober contribution!

--- projects/DnD Dice One Or More/main.py
+++ projects/DnD Dice One Or More/main.py
@@ -1,16 +1,33 @@
 import random
 
 def roll_the_dice():
-    valid_dice = (4, 6, 8, 10, 12, 20, 100)
-    # Ask user for the type of they want!
-    dice_type = int(input("Pick a Dice type: \n1. D4\n2. D6\n3. D8\n4. D10\n5. D12\n6. D20\n7. D100\n\n"))
-    dice_amount = int(input("How many dice do you want to roll? (One or more.)\n\n"))
-    try: 
+    # Set a variable for checking valid input
+    valid_input = False
+
+    # Loop asking the user for input as long as the input is invalid.
+    while not valid_input:
+        try:
+            # Ask user for the type of they want!
+            dice_type = int(input("Pick a Dice type: \n1. D4\n2. D6\n3. D8\n4. D10\n5. D12\n6. D20\n7. D100\n\n"))
+            # Ask user how many dice they want to roll
+            dice_amount = int(input("How many dice do you want to roll? (One or more.)\n\n"))
+
+            # Check if the input is valid and setting the variable to True to stop a new loop from happening
+            if dice_type not in range(1,8) or dice_amount <= 0:
+                print("Invalid input! Choose a valid dice type and a valid amount of dice!")
+            else:
+                valid_input = True
+                print("\nYou rolled:") 
+        except ValueError:
+            print("Invalid input. Choose a valid dice type and a valid amount of dice!")
+
+        # Match the dice type with the input and print a random int for each of the dice the user wanted to roll
         match dice_type:
             case 1:
                 for i in range(dice_amount):
                     number = random.randint(1, 4)
                     print(number)
+                break
             case 2:
                 for i in range(dice_amount):
                     number = random.randint(1, 6)
@@ -27,28 +44,22 @@ def roll_the_dice():
                 for i in range(dice_amount):
                     number = random.randint(1, 12)
                     print(number)
+            # Added the extra text for the Nat 1 and Nat 20 for flavor!
             case 6:
-               for i in range(dice_amount):
-                    number = random.randint(1, 20)
-                    if number == 1:
-                        print("Natural 1!")
-                    elif number == 20:
-                        print("Natural 20!")
-                    else:
-                        print(number)
+                for i in range(dice_amount):
+                        number = random.randint(1, 20)
+                        if number == 1:
+                            print("Natural 1!")
+                        elif number == 20:
+                            print("Natural 20!")
+                        else:
+                            print(number)
             case 7:
                 for i in range(dice_amount):
                     number = random.randint(1, 100)
                     print(number)
-            case _:
-                print("Not a valid option!")
-                exit()
-
-    except ValueError:
-        print("Invalid input. Choose a valid dice type and a valid amount of dice!")
 
 
-
-
+# Calling the function
 roll_the_dice()
 
