af783b5fcafbb01c75b297d42673637f549a8354
Lint Action <lint-action@samuelmeuli.com>
2023-10-25 17:38:50 +0000

    Fix code style issues with Black

--- projects/SMS_ChatBot/main.py
+++ projects/SMS_ChatBot/main.py
@@ -8,20 +8,18 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
-@app.route("/sms", methods=['POST'])
+
+@app.route("/sms", methods=["POST"])
 def chatgpt():
     # Get the incoming SMS message
-    inb_msg = request.form['Body'].lower()
+    inb_msg = request.form["Body"].lower()
 
     # Print the incoming message for debugging
     print(inb_msg)
 
     # Use the OpenAI API to generate a response
     response = openai.Completion.create(
-        model="gpt-3.5-turbo",
-        prompt=inb_msg,
-        max_tokens=3000,
-        temperature=0.7
+        model="gpt-3.5-turbo", prompt=inb_msg, max_tokens=3000, temperature=0.7
     )
 
     # Create a response using Twilio's MessagingResponse
@@ -29,6 +27,7 @@ def chatgpt():
     resp.message(response["choices"][0]["text"])
 
     return str(resp)
+
 
 if __name__ == "__main__":
     # Run the Flask app in debug mode
--- projects/Word_Predictor/main.py
+++ projects/Word_Predictor/main.py
@@ -18,6 +18,7 @@ Requirements:
 
 import pandas as pd
 
+
 class WhatsAppChat:
     def __init__(self, filename):
         # Initialize the object by reading in the chat file, and counting word frequency and next words
@@ -44,8 +45,9 @@ class WhatsAppChat:
                     else:
                         word_freq_dict[word] = 1
         # Sort the word frequency dictionary by value in descending order and return it
-        sorted_word_freq_dict = dict(sorted(word_freq_dict.items(),
-                                             key=lambda x: -1*int(x[1])))
+        sorted_word_freq_dict = dict(
+            sorted(word_freq_dict.items(), key=lambda x: -1 * int(x[1]))
+        )
         return sorted_word_freq_dict
 
     def find_next_words(self):
@@ -57,7 +59,7 @@ class WhatsAppChat:
             for i in range(len(words)):
                 if i != len(words) - 1:
                     current_word = words[i]
-                    next_word = words[i+1].replace("\n", "")
+                    next_word = words[i + 1].replace("\n", "")
                     if current_word in next_words_dict:
                         if next_word in next_words_dict[current_word]:
                             next_words_dict[current_word][next_word] += 1
@@ -80,20 +82,21 @@ class NextWordPredictor:
         if current_word in self.chat.next_words_dict:
             # Get the dictionary of next words for the current word and sort it by frequency
             next_word_freq_dict = self.chat.next_words_dict[current_word]
-            sorted_next_words = sorted(next_word_freq_dict.items(),
-                                        key=lambda x: x[1],
-                                        reverse=True)
+            sorted_next_words = sorted(
+                next_word_freq_dict.items(), key=lambda x: x[1], reverse=True
+            )
             # Get the top num_predictions words and return them
-            next_words = [word[0] for word in sorted_next_words[:self.num_predictions]]
+            next_words = [word[0] for word in sorted_next_words[: self.num_predictions]]
             return next_words
         else:
             # If the current word is not in the dictionary of next words, return None
             return None
 
+
 # Create a WhatsAppChat object and a NextWordPredictor object after loading Chats.txt file
 # set n as the length of words you want to predict after a given word
-chat = WhatsAppChat('Chats.txt')
-n, given_word = 3, 'good'
+chat = WhatsAppChat("Chats.txt")
+n, given_word = 3, "good"
 predictor = NextWordPredictor(chat, num_predictions=n)
 
 # This prints the next n word predictions after the given_word
