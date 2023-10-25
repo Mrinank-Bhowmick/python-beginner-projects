2be7f0df0870fc7f6bcf03758f13105a691ac0af
Lint Action <lint-action@samuelmeuli.com>
2023-10-25 17:31:47 +0000

    Fix code style issues with Black

--- projects/Book Data Extractor/main.py
+++ projects/Book Data Extractor/main.py
@@ -1,5 +1,3 @@
-
-
 from bs4 import BeautifulSoup as bs
 import requests
 from openpyxl import Workbook
@@ -14,63 +12,67 @@ ws["A1"].value = "Title"
 ws["B1"].value = "Author"
 ws["C1"].value = "Ratings"
 
+
 def link(url, pages):
-    for i in range(1, pages+1):
-         title = []
-         author = []
-         rating = []
-         try:
-            response =  requests.get(f'{url}?page={i}')
-         except:
-             print("Tracked All The Pages! ")
-             break
+    for i in range(1, pages + 1):
+        title = []
+        author = []
+        rating = []
+        try:
+            response = requests.get(f"{url}?page={i}")
+        except:
+            print("Tracked All The Pages! ")
+            break
 
-         html = response.text
-         data = bs(html, "html.parser")
+        html = response.text
+        data = bs(html, "html.parser")
 
-         titles = data.find_all("span", itemprop="name", role="heading")
-         for j in titles:
-             title.append(j.string)
+        titles = data.find_all("span", itemprop="name", role="heading")
+        for j in titles:
+            title.append(j.string)
 
-         authors = data.find_all("a", class_="authorName")
-         for j in authors:
-             author.append(j.string)
+        authors = data.find_all("a", class_="authorName")
+        for j in authors:
+            author.append(j.string)
 
-         ratings = data.find_all("span", class_="minirating")
-         for j in ratings:
-             rating.append(j.get_text().replace(" ", "")[0:4])
+        ratings = data.find_all("span", class_="minirating")
+        for j in ratings:
+            rating.append(j.get_text().replace(" ", "")[0:4])
 
-         for j in range(len(title)):
-             try:
+        for j in range(len(title)):
+            try:
                 rating_float = float(rating[j].strip())
                 if rating_float >= rating_threshold:
-                   ws.append([title[j], author[j], rating_float])
-             except ValueError:
-                     print(f"Skipping item with invalid rating: '{rating[j]}'")
+                    ws.append([title[j], author[j], rating_float])
+            except ValueError:
+                print(f"Skipping item with invalid rating: '{rating[j]}'")
 
-         print(f"Successfully extracted page {i}")
+        print(f"Successfully extracted page {i}")
 
 
 if __name__ == "__main__":
-
     try:
-       url = input("Enter the exact link of the book list from  the goodreads website: ")
+        url = input(
+            "Enter the exact link of the book list from  the goodreads website: "
+        )
     except:
         print("Enter a Valid Link! ")
 
     while True:
-          try:
-            rating_threshold = float(input("Enter the minimum ratings you want to extract: "))
+        try:
+            rating_threshold = float(
+                input("Enter the minimum ratings you want to extract: ")
+            )
             break
-          except ValueError:
+        except ValueError:
             print("Enter a valid number")
 
     while True:
-          try:
-              pages = int(input("Enter the total number of Web Pages in Your List: "))
-              break
-          except ValueError:
-              print("Please input a numerical value! ")
+        try:
+            pages = int(input("Enter the total number of Web Pages in Your List: "))
+            break
+        except ValueError:
+            print("Please input a numerical value! ")
 
     print("Extracting the Data..........................")
 
--- projects/Text Summarizer/summarizer.py
+++ projects/Text Summarizer/summarizer.py
@@ -1,18 +1,19 @@
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
+
 stop_words = list(STOP_WORDS)
-nlp = spacy.load('en_core_web_md')
+nlp = spacy.load("en_core_web_md")
 
 
 def summarize(text):
-    punctuation = ''
+    punctuation = ""
     document = nlp(text)
     len(document)
     tokens = [token.text for token in document]
     print(tokens)
 
-    punctuation = punctuation+'\n'
+    punctuation = punctuation + "\n"
     print(punctuation)
 
     wordfreq = {}
@@ -27,7 +28,7 @@ def summarize(text):
 
     word_freq = max(wordfreq.values())
     for word in wordfreq.keys():
-        wordfreq[word] = wordfreq[word]/word_freq
+        wordfreq[word] = wordfreq[word] / word_freq
     sentence = [sent for sent in document.sents]
     print(sentence)
 
@@ -41,15 +42,16 @@ def summarize(text):
                     sentence_score[sent] += wordfreq[word.text.lower()]
 
     from heapq import nlargest
-    summary_length = int(len(sentence)*0.3)
+
+    summary_length = int(len(sentence) * 0.3)
     print(sentence_score)
 
     summary = nlargest(summary_length, sentence_score, key=sentence_score.get)
     final_summary = [word.text for word in summary]
-    summary = ' '.join(final_summary)
+    summary = " ".join(final_summary)
 
     print(summary)
 
 
-text = input('Enter the text: ')
+text = input("Enter the text: ")
 summarize(text)
