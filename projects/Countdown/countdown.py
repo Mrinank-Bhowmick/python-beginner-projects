db64b45550ee239317d91f023a7e2be183e5db1c
Sae Inoue <flower.smile120717@gmail.com>
2023-12-06 10:46:40 +0800

    add documentation and improve the naming

--- projects/Countdown/countdown.py
+++ projects/Countdown/countdown.py
@@ -1,20 +1,24 @@
 import time
 
 
-def countdown(time_sec):
-    while time_sec:
-        mins, secs = divmod(time_sec, 60)
-        timeformat = "{:02d}:{:02d}".format(mins, secs)
-        print(timeformat, end="\r")
+def countdown_timer(time_span__seconds): #"countdown is not specifc enough."
+    '''
+    Argument:
+    time_span__seconds(int):Time duration in seconds.
+    '''
+    while time_span__seconds:
+        mins, secs = divmod(time_span__seconds, 60) #mins = duration_seconds // 60, secs = duration_seconds % 60
+        time_format = "{:02d}:{:02d}".format(mins, secs)
+        print(time_format, end="\r")
         time.sleep(1)
-        time_sec -= 1
+        time_span__seconds -= 1
 
     print("Time Up")
 
 
 if __name__ == "__main__":
     try:
-        t = input("Enter the time in seconds: ")
-        countdown(int(t))
+        user_input = input("Enter the time in seconds: ") # t has no meaning.
+        countdown_timer(int(user_input))
     except:
-        print("Invalid input please try again...")
+        print("Invalid input please enter a valid number...")
