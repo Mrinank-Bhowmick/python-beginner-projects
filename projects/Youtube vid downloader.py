d845b3823590f8a4221667d83b125ffd7f66559d
Akarn-26 <134209517+Akarn-26@users.noreply.github.com>
2023-12-31 12:00:10 +0545

    Delete projects/Youtube vid downloader.py

--- projects/Youtube vid downloader.py
+++
@@ -1,5 +0,0 @@
-# This is a simple python file which can download videos easy,fast and hassle free
-from y2mate_api import Handler
-api=Handler('Alan Walker- Faded') # You can  write the Title of the song or a video 
-api.auto_save(format='mp4' , quality='1080p') #mentioning the format and quality of the video of the
-#Run the command and you'll see your donwload process bar ahead
