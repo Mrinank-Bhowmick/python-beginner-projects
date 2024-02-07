356f2585e5a2f731d7e1837c9495c4d8f0f716fe
Xia <157871580+Xia3412@users.noreply.github.com>
2024-02-08 03:46:01 +1100

    Made some changes to the comments

--- projects/Discord Bot/main.py
+++ projects/Discord Bot/main.py
@@ -11,8 +11,6 @@ bot = discord.bot(comand_prefix="!")
 
 # The list below is a list that contains all the responses the bot can randomly choose from for the "ask" command
 # Scroll down to see the "ask" command's functionality
-# It is recommended to make this list in a different .txt file or .py file and then importing it to this main.py
-# file but for the sake of simplicity, I have added this list here only in the main bot file.
 
 responses_list = ["It is certain",
                   "It is decidedly so",
@@ -63,23 +61,13 @@ async def ask(ctx: commands.Context, *, question: str):
 
 # Note:- If we don't put a ( * ) before the question paramter, the bot will only take the first word from the user
 # input. For example: Running the command like this:- "!ask how are you?"
-# The bot will read that command as:- "how". To avoid this, we add an asterisk( * ) before the question parameter
-# so the bot will read the command as "how are you?".
+# The bot will read that command as:- "how"
     
     await ctx.reply(f"{ctx.author.mention} asks: **{question}**\nMy reply: **{random.choice(responses_list)}**")
 
-# the ctx object has an attribute called "reply" which will reply to the user's message when the user uses to command
-# prefix along with the command name. And the rest of the code is self explanatory. The "\n" basically makes it so
-# that the rest of the code after that "\n" will be printed in a new line. So ultimately, running this command
-# on discord will look like this:-
-    
-    # lets say user asks the bot "how are you". So the user will send: "!ask how are you".
-    # the bot will reply with "{user's discord id mention} asks: how are you"
-    #                         "My reply: {a random response from the responses list}"  
-
 # There's one last thing to do now.. which is handling error. As we can see, the "ask" commands needs a question
 # parameter.. but what if the user just uses the command and never provide the bot with the question parameter?
-# This situation will through an error called "MissingRequiredArgument". In order to avoid this, we can locally
+# This situation will throw an error called "MissingRequiredArgument". In order to avoid this, we can locally
 # create a error handler for this "ask" command. You can also use a try and except block to catch the error.
     
 @ask.error
