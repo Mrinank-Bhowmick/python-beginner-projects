5a086ce12648c1e306293282a99b31df2029baab
novat <nova.thanos@gmail.com>
2024-02-11 19:37:39 +0200

    fixed the issue  #650

--- projects/Higher-Lower/main.py
+++ projects/Higher-Lower/main.py
@@ -1,13 +1,26 @@
 from random import randint
-
+print("Welcome to the Higher-Lower Game!")
 rnum = randint(0, 100)
 noguesses = 0
 while True:
-    guess = int(input(": "))
-    if guess > rnum:
-        print("Lower")
-    elif guess < rnum:
-        print("Higher")
-    else:
-        (print("You win ({noguesses} guesses used)"), quit())
-    noguesses += 1
+
+    while True:
+        guess = input("Guess the number: ")
+        if guess.isdigit():
+            # Check if the input string consists of digits only
+            integer_number = int(guess)
+            print("You entered:", integer_number)
+
+            if integer_number > rnum:
+                print("Lower")
+            elif integer_number < rnum:
+                print("Higher")
+            else:
+                (print("You win! the number is " +guess+"!"), quit())
+            noguesses += 1
+        else:
+            print("Invalid input. Please enter an integer number.")
+
+
+
+
