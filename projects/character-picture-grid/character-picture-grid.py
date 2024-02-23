b591dc7c2c4753ff0ba50777b1eae919b5f530ee
ibra-kdbra <git-repo-develop@outlook.com>
2024-02-23 23:33:54 +0300

    rotates a grid 90 degrees

---
+++ projects/character-picture-grid/character-picture-grid.py
@@ -0,0 +1,36 @@
+def rotate90(grid):
+    """Rotates a grid 90 degrees
+    Args:
+        grid (list): a 2d list representing a grid
+    Returns:
+        grid (list): rotated copy of a 2d grid
+    """
+    return list(zip(*grid[::-1]))
+
+def print2DGrid(grid):
+    """Prints a 2D grid
+    Args:
+        grid (list): 2D grid
+    Returns:
+        None
+    """
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            print(grid[row][col], end='')
+
+        print()
+
+if __name__ == "__main__":
+
+    grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+    gridRotated = rotate90(grid)
+    print2DGrid(gridRotated)
