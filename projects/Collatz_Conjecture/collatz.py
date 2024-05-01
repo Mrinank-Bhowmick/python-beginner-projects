50a339626db523c252ce54df3f7afcb25e3b0bc7
rakinplaban <rakinshahriar54@gmail.com>
2024-05-01 11:43:18 +0600

    Added necessary comments on python code.

--- projects/Collatz_Conjecture/collatz.py
+++ projects/Collatz_Conjecture/collatz.py
@@ -1,13 +1,19 @@
+# Defined a class to generate Collatz Sequence
 class Collatz:
+    # Constructor to initialize the number
     def __init__(self, n):
         self.n = n
 
+    # Iteration method
     def __iter__(self):
         return self
 
+    # Next method to generate next number in the sequence
     def __next__(self):
         if self.n == 1:
+            # Stop iteration when the number is 1
             raise StopIteration
+        
         if self.n % 2 == 0:
             self.n = self.n // 2
         else:
@@ -15,11 +21,11 @@ class Collatz:
         return self.n
     
 
-
+# Main function to test the class
 if __name__ == '__main__':
-    print("To generate a Collatz Sequence, please provide an integer positive number")
+    print("To generate a Collatz Sequence,")
     try:
-        input_number = int(input("Please enter a number: "))
+        input_number = int(input("Please enter a positive integer number: "))
         for i in Collatz(input_number):
             print(i, end=" ")
     except StopIteration:
