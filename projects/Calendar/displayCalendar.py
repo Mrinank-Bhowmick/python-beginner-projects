397ddd0aca06fcae0c76967583371dd3406e0e35
yest3a <volcanoapple7@gmail.com>
2024-05-15 14:35:30 +0800

    changed variable names to enforce better coding practices

--- projects/Calendar/displayCalendar.py
+++ projects/Calendar/displayCalendar.py
@@ -1,49 +1,46 @@
-# Program to display calendar
-
-
-def display_calendar(year, month):
+def display_cal(year_input, month_input):
     """
-    Display a calendar for the given year and month.
+    Display a calendar for the desired year and month.
 
     Parameters:
-    year (int): The year for which the calendar is to be displayed.
-    month (int): The month for which the calendar is to be displayed.
+    year_input (int): The year for which the calendar is to be displayed.
+    month_input (int): The month for which the calendar is to be displayed.
 
     """
     import calendar
 
-    print(calendar.month(year, month))
+    print(calendar.month(year_input, month_input))
 
 
-def get_valid_year():
+def fetch_year():
     """
-    Function to prompt the user for a valid year input and return the valid year as an integer.
+    Prompts the user for a valid year and returns the year as an integer.
     """
     while True:
         try:
-            year = int(input("Enter year: "))
-            if year < 0:
+            year_input = int(input("Enter year: "))
+            if year_input < 0:
                 raise ValueError("Year must be a positive integer")
-            return year
+            return year_input
         except ValueError:
             print("Invalid input. Please enter a valid year.")
 
 
-def get_valid_month():
+def fetch_month():
     """
-    A function that prompts the user to enter a month, validates the input, and returns the valid month.
+    Function that asks the user to enter a month, validates the input, and returns the valid month.
     """
     while True:
         try:
-            month = int(input("Enter month: "))
-            if month < 1 or month > 12:
+            month_input = int(input("Enter month: "))
+            if month_input < 1 or month_input > 12:
                 raise ValueError("Month must be between 1 and 12")
-            return month
+            return month_input
         except ValueError:
             print("Invalid input. Please enter a valid month.")
 
 
-year = get_valid_year()
-month = get_valid_month()
+year_input = fetch_year()
+month_input = fetch_month()
 
-display_calendar(year, month)
+display_cal(year_input, month_input)
