9d5f200a47080ad8d69af2cef71e4e7ee3196b04
Alby08 <aidenej@hotmail.com>
2024-05-18 22:30:57 +0800

    Feat: Add and Update code comments

--- projects/Chat-GPT-Discord-Bot/main.py
+++ projects/Chat-GPT-Discord-Bot/main.py
@@ -23,30 +23,32 @@ class MyClient(discord.Client):
         super().__init__(intents=intents)
         # A CommandTree is a special type that holds all the application command
         # state required to make it work. This is a separate class because it
-        # allows all the extra state to be opt-in.
+        # allows all the extra states to be opt-in.
         # Whenever you want to work with application commands, your tree is used
         # to store and work with them.
         # Note: When using commands.Bot instead of discord.Client, the bot will
         # maintain its own tree instead.
         self.tree = app_commands.CommandTree(self)
 
-    # In this basic example, we just synchronize the app commands to one guild.
+    # In this, we just synchronize the app commands to specified guilds.
     # Instead of specifying a guild to every command, we copy over our global commands instead.
     # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+    # This allows for faster bot testing and development.
     async def setup_hook(self):
-        # This copies the global commands over to your guild.
+        # This copies the global commands over to your guild(s).
         self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
         self.tree.copy_global_to(guild=MY_GUILD_2)
         await self.tree.sync(guild=MY_GUILD_2)
 
-    async def on_message(self, message):
+    async def on_message(self, message): # Whenever a user sends a message it is logged into the console
         try:
             print(f'Message from {message.author}({message.author.id}) in {message.guild}({message.channel.name}): {message.content}')
             if message.author.id == self.user.id:
                 return
         except AttributeError:
-            print("Unable to print logged user message 'DMChannel' error (Cant log sent commands)") # This isnt needed. you can remove this print() statement if you want. I like it for debug purposes
+            print("Unable to print logged user message 'DMChannel' error (Cant log sent commands)") # This print() statement isnt needed. 
+                                                                                                    # you can remove it if you want. I like it for debug purposes
             return
             
 
@@ -60,24 +62,23 @@ client = MyClient(intents=intents)
 async def on_ready():
     print(f"Logged in as {client.user} (ID: {client.user.id})")
     print("-------------------------------")
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='For Slash Commands'))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='For Slash Commands')) # This changes the activity that is displayed
+                                                                                                                           # under the bots name in the members list.
 
 
-@client.tree.command(name="test_bot_is_running", description="Replies with hello!")
+@client.tree.command(name="test_bot", description="Replies with 'Hello!'")
 async def running_test(interaction: discord.Interaction):
-    # noinspection PyUnresolvedReferences
-    await interaction.response.send_message(f"Hello, {interaction.user.mention}", ephemeral=True)
+    await interaction.response.send_message(f"Hello, {interaction.user.mention}", ephemeral=True) # ephemeral=True means the bots response is only visible
+                                                                                                  # to the user who used the command. Also acts like response.defer 
+                                                                                                  # and stops command timeout
 
 
-@client.tree.command(name="shutdown", description="Shuts down the bot")
+@client.tree.command(name="shutdown", description="Shuts down the bot") # Shuts down the bot if the user matches the owner_uid
 async def shutdown_bot(interaction: discord.Interaction):
-    # Replace 'YOUR_USER_ID' with your actual Discord user ID
     if interaction.user.id == owner_uid:
-        # noinspection PyUnresolvedReferences
         await interaction.response.send_message("Shutting down the bot...")
         await client.close()
     else:
-        # noinspection PyUnresolvedReferences
         await interaction.response.send_message("You don't have permission to shut down the bot.", ephemeral=True)
 
 
@@ -86,13 +87,10 @@ async def shutdown_bot(interaction: discord.Interaction):
 @app_commands.describe(text_to_send="Text to send in the current channel")
 async def send(interaction: discord.Interaction, text_to_send: str):
     if "@everyone" in text_to_send:
-        # noinspection PyUnresolvedReferences
         await interaction.response.send_message("Nice try 😉", ephemeral=True)
     elif "@here" in text_to_send:
-        # noinspection PyUnresolvedReferences
         await interaction.response.send_message("Nice try 😉", ephemeral=True)
     else:
-        # noinspection PyUnresolvedReferences
         await interaction.response.send_message(text_to_send)
 
 
@@ -100,7 +98,6 @@ async def send(interaction: discord.Interaction, text_to_send: str):
 @app_commands.rename(to_delete="messages")
 @app_commands.describe(to_delete="Number of messages to delete")
 async def send(interaction: discord.Interaction, to_delete: int):
-    # noinspection PyUnresolvedReferences
     await interaction.response.defer(ephemeral=True)
     if not interaction.user.guild_permissions.manage_messages:
         await interaction.followup.send("Invalid permissions")
@@ -109,7 +106,6 @@ async def send(interaction: discord.Interaction, to_delete: int):
         await interaction.followup.send("Invalid number")
         return
     else:
-        # noinspection PyUnresolvedReferences
         await interaction.followup.send("Deleting...")
         await interaction.channel.purge(limit=to_delete)
         await interaction.edit_original_response(content=f"Deleted {to_delete} messages.")
@@ -120,13 +116,10 @@ async def send(interaction: discord.Interaction, to_delete: int):
                        second_value="The value you want to add to the first value")
 async def add(interaction: discord.Interaction, first_value: int, second_value: int):
     try:
-        # noinspection PyUnresolvedReferences
         await interaction.response.defer(ephemeral=True, thinking=False)
-        # noinspection PyUnresolvedReferences
         await interaction.followup.send(f"{first_value} + {second_value} = {first_value + second_value}")
     except Exception as e:
         await asyncio.sleep(2)
-        # noinspection PyUnresolvedReferences
         print(f"An error occurred: {str(e)}")
         await interaction.followup.send("An error occurred while processing the command.")
 
@@ -134,16 +127,15 @@ async def add(interaction: discord.Interaction, first_value: int, second_value: 
 @client.tree.command(name="ping", description="Get bot latency")
 async def ping(interaction: discord.Interaction):
     try:
-        # noinspection PyUnresolvedReferences
-        await interaction.response.defer(ephemeral=True)  # Defer the response
+        await interaction.response.defer(ephemeral=True)  # Defer the response to prevent command timeout
 
-        # Get the bot's latency
+        # Get bot latency
         latency = round(client.latency * 1000)
 
-        # Send the ping as a followup message
+        # Send as followup message
         await interaction.followup.send(f"Pong! Latency: {latency}ms")
     except Exception as e:
-        # Handle any exceptions
+        # Handle exceptions
         print(f"An error occurred: {str(e)}")
         await interaction.followup.send("An error occurred while processing the command.")
 
