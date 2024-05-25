9884f59e11927c60ec9b0063aa1948e8c52f48f0
justinjohnsondev <jjustin634@gmail.com>
2024-05-25 01:50:14 -0500

    Adding comment to __init__ for clarity

--- projects/url_shortener/app/services/url_shortener.py
+++ projects/url_shortener/app/services/url_shortener.py
@@ -6,24 +6,28 @@ from pydantic import BaseModel, PrivateAttr, ValidationError
 
 
 class URLShortener(BaseModel):
-	url: str
-	_shortener: Optional[pyshorteners.Shortener] = PrivateAttr()
+    url: str
+    _shortener: Optional[pyshorteners.Shortener] = PrivateAttr()
 
-	class Config:
-		arbitrary_types_allowed = True
+    class Config:
+        arbitrary_types_allowed = True
 
-	def __init__(self, **data):
-		shortener = data.pop("shortener", None) or pyshorteners.Shortener()
-		super().__init__(**data)
-		self._shortener = shortener
+    def __init__(self, **data):
+        """
+        Initialize the URLShortener. If no 'shortener' is provided in data,
+        a new pyshorteners.Shortener instance is created.
+        """
+        shortener = data.pop("shortener", None) or pyshorteners.Shortener()
+        super().__init__(**data)
+        self._shortener = shortener
 
-	def shorten_url(self) -> Union[str, Exception]:
-		if self._shortener is None:
-			raise ValueError("Shortener is not initialized.")
+    def shorten_url(self) -> Union[str, Exception]:
+        if self._shortener is None:
+            raise ValueError("Shortener is not initialized.")
 
-		try:
-			return str(self._shortener.tinyurl.short(self.url))
-		except (ParsingError, ValidationError) as error:
-			return error
-		except Exception as generic_error:
-			return generic_error
+        try:
+            return str(self._shortener.tinyurl.short(self.url))
+        except (ParsingError, ValidationError) as error:
+            return error
+        except Exception as generic_error:
+            return generic_error
