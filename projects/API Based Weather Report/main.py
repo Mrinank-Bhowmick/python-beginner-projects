548de70d0443b9b21da286edcc9cb5a5c3a992d7
rizaghoshmitra <20111569@tafe.wa.edu.au>
2024-06-04 11:25:30 +0800

    fix: issue 730 - Invalid location handle

--- projects/API Based Weather Report/main.py
+++ projects/API Based Weather Report/main.py
@@ -8,16 +8,18 @@ def main():
     print("You can obtain your API key by signing up at https://home.openweathermap.org/users/sign_up")
     api_key = input("Please enter your OpenWeatherMap API key: ")
     location = input("Enter the city name: ")
-    weather_data = fetch_weather(api_key, location)
-    if weather_data:
+    status_code, weather_data = fetch_weather(api_key, location)
+    if status_code == 200:
         write_to_file(location, weather_data)
         print("Current temperature is: {:.2f} °C".format(weather_data["main"]["temp"] - 273.15))
         print("Current weather desc  : " + weather_data["weather"][0]["description"])
         print("Current Humidity      :", weather_data["main"]["humidity"], "%")
         print("Current wind speed    :", weather_data["wind"]["speed"], "kmph")
         print("Country Code          :", weather_data["sys"]["country"])
+    elif 400 <= status_code < 500:  # Check if status code is in the 4xx range (Client error)
+        print(f"Failed to fetch weather data. Client error: Status Code {status_code} , message : {weather_data["message"]}")
     else:
-        print("Failed to fetch weather data. Please check your input and try again.")
+        print(f"Failed to fetch weather data. Server error: Status Code {status_code}")
 
 if __name__ == "__main__":
     main()
