77e871db78722f95f8665c6831132da18e57adfd
unknown <yunjasgu@gmail.com>
2024-06-06 22:43:19 +0800

    Add docstring in each test file

--- projects/Calculate Age/tests/test_calculate_age.py
+++ projects/Calculate Age/tests/test_calculate_age.py
@@ -4,12 +4,21 @@ from calculate_age import age_calculator
 
 @patch('time.time', return_value=1621848668.0)
 def test_age_calculator(mock_time):
+    """
+     Test the age_calculator function
+     Mocks the current time and check if the age is calculated
+     based on current time
+    """
     name = "Chloe"
     age = 30
     expect_output = "Chloe's age is 30 years or 365 months or 11102 days"
     assert age_calculator(name, age) == expect_output
 
 def test_age_calculator_negative_age():
+    """
+    Tests the age_calculator function for negative age input
+    Check for ValueError when user input negative age.
+    """
     name = "Emma"
     age = -5
     try:
@@ -19,6 +28,10 @@ def test_age_calculator_negative_age():
 
 
 def test_age_calculator_leap_year():
+    """
+    Test the age_calculator function considering leap years
+    Check for expect_output_leap_year comparing the real output considering leap years
+    """
     name = "David"
     age = 30
     expect_output_leap_year = "David's age is 30 years or 366 months or 11115 days"
--- projects/Calculate Age/tests/test_utilize_date.py
+++ projects/Calculate Age/tests/test_utilize_date.py
@@ -3,6 +3,11 @@ from utilize_date import judge_leap_year, month_days
 
 
 def test_judge_leap_year():
+    """
+     judge_leap_year function tests whether a given year is a leap year.
+
+     A leap year is divisible by 4 and, if divisible by 100, must also be divisible by 400.
+    """
     assert judge_leap_year(2000) == True
     assert judge_leap_year(2008) == True
     assert judge_leap_year(2023) == False
@@ -12,6 +17,11 @@ def test_judge_leap_year():
 
 
 def test_month_days():
+    """
+    The month_days function tests whether if returns the correct number of days for a given month
+
+    For Feb, both leap years and common years are considered.
+    """
     assert month_days(7, False) == 31
     assert month_days(4, True) == 30
     assert month_days(2, True) == 29
