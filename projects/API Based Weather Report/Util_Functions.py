dccb69f6bd7680571a1e895ddf01ee830ba3fd22
yanliutafewa <20109301@tafe.wa.edu.au>
2024-06-07 20:04:24 +0800

    feat: user can choose to output temperature either in Celsius or Fahrenheit.

--- projects/API Based Weather Report/Util_Functions.py
+++ projects/API Based Weather Report/Util_Functions.py
@@ -12,7 +12,7 @@ def wind_degree_to_direction(str_wind_degree):
     try:
         wind_degree = int(str_wind_degree)
     except ValueError:
-        return "API Wind Degree data error!"
+        return "API Wind Degree data format error!"
 
     directions = [
         "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE",
@@ -34,12 +34,12 @@ def unix_timestamp_to_localtime(str_unix_timestamp, str_timezone_offset_seconds)
     try:
         unix_timestamp = int(str_unix_timestamp)
     except ValueError:
-        return "API sunset/sunrise data error!"
+        return "API sunset/sunrise data format error!"
 
     try:
         timezone_offset_seconds = int(str_timezone_offset_seconds)
     except ValueError:
-        return "API timezone data error!"
+        return "API timezone data format error!"
 
     # Convert Unix timestamp to UTC datetime
     utc_time = datetime.utcfromtimestamp(unix_timestamp)
@@ -48,3 +48,32 @@ def unix_timestamp_to_localtime(str_unix_timestamp, str_timezone_offset_seconds)
     local_time = utc_time + timedelta(seconds=timezone_offset_seconds)
 
     return local_time.strftime('%Y-%m-%d %H:%M:%S')
+
+
+def convert_temperature(str_temperature_kelvin, temperature_unit):
+    """
+    Convert temperature in Kelvin degree to Celsius degree or Fahrenheit degree based on second parameter .
+
+    :param str_temperature_kelvin: str, temperature in Kelvin degree (e.g., "291.19")
+    :param temperature_unit: str, "C" for Celsius, "F" for Fahrenheit
+    :return: temperature (e.g., "21.07 °C" or "67.12 °F")
+    """
+    # Convert strings to integers
+    try:
+        temperature_k = int(str_temperature_kelvin)
+    except ValueError:
+        return "API temperature data format error!"
+
+    # Validate temperature_unit
+    unit = str(temperature_unit).upper()
+    if not (unit == "C" or unit == "F"):
+        return "Temperature unit must either be 'C' or be 'F'!"
+
+    # Converting
+    if unit == 'C':
+        temperature_c = temperature_k - 273.15
+        return f"{temperature_c:.2f} °C"
+
+    if unit == 'F':
+        temperature_f = temperature_k * 9 / 5 - 459.67
+        return f"{temperature_f:.2f} °F"
