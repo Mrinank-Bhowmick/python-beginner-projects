909b902910e9eccf69bea4ff3837b32babd61a71
Sae Inoue <flower.smile120717@gmail.com>
2024-06-09 13:57:29 +0800

    feat:Add show answer functiont to show correct answers Add constructor init into the system to initialize the object

--- projects/Quiz Game/src/gameplay.py
+++ projects/Quiz Game/src/gameplay.py
@@ -1,19 +1,22 @@
-import gamesystem as game
+from gamesystem import QuizGameSystem
 
-game.welcome_message()
-start = game.start_game()
+def main():
+    game = QuizGameSystem()
+    game.welcome_message()
+    start = game.start_game()
 
-if(start):
-    score = 0
-    total_questions = len(game.get_questions())
+    if (start):
+        score = 0
+        total_questions = len(game.get_questions())
 
-    for question in game.get_questions():
-        score += game.ask_question(question["question"], question["answer"])
+        for question in game.get_questions():
+            score += game.ask_question(question["question"], question["answer"])
+
+        game.show_result(score, total_questions)
+
+    else:
+        print("See you next time")
 
 
-    game.show_result(score,total_questions)
-
-else:
-    print("See you next time")
-
-
+if __name__ == "__main__":
+    main()
