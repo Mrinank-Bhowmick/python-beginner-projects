97efc4c86a97eaa43ae5f8eb674c96486a703811
Dinesh Kartik R <rdineshkartikdk@gmail.com>
2025-01-05 16:24:46 +0530

    Just added comments

--- projects/Desktop-Notification/main.py
+++ projects/Desktop-Notification/main.py
@@ -3,8 +3,8 @@ from plyer import notification
 
 # Display a notification with an icon
 notification.notify(
-    title="Python Project",
-    message="Here is your notification body",
+    title="Python Project", # This is the Title
+    message="Here is your notification body", # This is the body of the notification
     app_icon="logo.ico",  # Replace with the path to your icon file
     timeout=10  # Duration in seconds
 )
